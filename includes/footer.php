2d679805ec936cd0a1baef8142df0873a8410275
Phil Ewels <phil.ewels@scilifelab.se>
2018-04-19 17:40:00 +0200

    Move header and footer out into PHP includes

---
+++ includes/footer.php
@@ -0,0 +1,45 @@
+
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-6 col-md">
+            <img height="30px" src="assets/img/logo/nf-core-logo.svg" />
+            <small class="d-block mb-3 text-muted">&copy; 2018</small>
+            <small class="d-block mb-3 text-muted">Feature icons made by <a href="http://www.freepik.com/" class="text-muted">Freepik</a> from <a href="http://www.flaticon.com/" class="text-muted">www.flaticon.com</a></small>
+          </div>
+          <div class="col-6 col-md">
+            <h5>Getting Started</h5>
+            <ul class="list-unstyled">
+              <li><a class="text-muted" href="#">Workflow features</a></li>
+              <li><a class="text-muted" href="#">Using nextflow</a></li>
+              <li><a class="text-muted" href="#">Available pipelines</a></li>
+            </ul>
+          </div>
+          <div class="col-6 col-md">
+            <h5>For Authors</h5>
+            <ul class="list-unstyled">
+              <li><a class="text-muted" href="#">Documentation</a></li>
+              <li><a class="text-muted" href="#">Error codes</a></li>
+              <li><a class="text-muted" href="#">Helper tools</a></li>
+            </ul>
+          </div>
+          <div class="col-6 col-md">
+            <h5>About nf-core</h5>
+            <ul class="list-unstyled">
+              <li><a class="text-muted" href="#">The history of the project</a></li>
+              <li><a class="text-muted" href="#">List of contributors</a></li>
+              <li><a class="text-muted" href="#">Where to get help</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('assets/js/jquery-slim.min.js"><\/script>')</script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/nf-core.js"></script>
+  </body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>nf-core</title>
+    <meta name="description" content="A collection of high quality Nextflow pipelines">
+    <meta name="author" content="Phil Ewels">
+    <link rel="shortcut icon" href="assets/img/logo/nf-core-logo-square.png" type="image/png" />
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/nf-core.css" rel="stylesheet">
+  </head>
+  <body>
+
+    <nav class="navbar fixed-top navbar-expand-md navbar-light site-nav">
+      <a class="navbar-brand d-md-none" href="#"><img height="25px" src="assets/img/logo/nf-core-logo.svg" /></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-md-center" id="navbarCollapse">
+        <ul class="navbar-nav">
+          <li class="nav-item p-1">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+          <li class="nav-item p-1">
+            <a class="nav-link" href="#">Pipelines</a>
+          </li>
+          <li class="nav-item p-1">
+            <a class="nav-link" href="#">Tools</a>
+          </li>
+          <li class="nav-item p-1">
+            <a class="nav-link" href="#">Docs</a>
+          </li>
+          <li class="nav-item p-1">
+            <a class="nav-link" href="#">About</a>
+          </li>
+        </ul>
+        <hr class="d-md-none">
+        <ul class="navbar-nav d-md-none">
+          <li class="nav-item p-1">
+            <a class="nav-link" target="_blank" href="https://gitter.im/nf-core/Lobby">Chat on Gitter</a>
+          </li>
+          <li class="nav-item p-1 mb-3">
+            <a class="nav-link" target="_blank" href="https://github.com/nf-core">See nf-core on GitHub</a>
+          </li>
+        </ul>
+        <div class="d-none d-md-block" style="position:absolute; right: 1rem;">
+          <a class="nav-link d-inline-block px-2" target="_blank" href="https://gitter.im/nf-core/Lobby" data-toggle="tooltip" title="Chat on Gitter"><img height="25px" src="assets/img/gitter.svg" /></a>
+          <a class="nav-link d-inline-block px-2" target="_blank" href="https://github.com/nf-core" data-toggle="tooltip" title="See nf-core on GitHub"><img height="25px" src="assets/img/github.svg" /></a>
+        </div>
+      </div>
+    </nav>
