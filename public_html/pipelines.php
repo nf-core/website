c0228b46e89168ccee2e1114123f5cbc1bf7b98a
Phil Ewels <phil.ewels@scilifelab.se>
2018-04-19 19:45:55 +0200

    Move hand-written page content to markdown files.

--- public_html/pipelines.php
+++ public_html/pipelines.php
@@ -4,6 +4,10 @@ $subtitle = 'Browse the XXX pipelines that are currently available as part of nf
 include('../includes/header.php');
 ?>
 
+<h2>Coming soon...</h2>
+
+<!--
+
 <h1>Available Pipelines</h1>
 <p>Can you think of another pipeline that would fit in well?
 <a href="https://github.com/nf-core/nf-core.github.io/issues/new">Let us know!</a></p>
@@ -13,5 +17,7 @@ include('../includes/header.php');
 <h2>Pipelines in development</h2>
 <p>These pipelines aren't yet ready for the prime-time, but are under active development.
 Once they've had a release on GitHub, they will be classed as production-ready pipelines.</p>
+
+-->
 
 <?php include('../includes/footer.php'); ?>
