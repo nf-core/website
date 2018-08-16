3599d3a694326e04c90676af8b6baed25a4ff07d
Johannes Alneberg <alneberg@kth.se>
2018-08-16 09:45:36 +0200

    Changed subtitle according to suggestion by @FranBonath

--- public_html/nextflow_tutorial.php
+++ public_html/nextflow_tutorial.php
@@ -1,6 +1,6 @@
 <?php
 $title = 'Nextflow tutorial';
-$subtitle = 'Learn how nf-core pipelines are automatically kept up to date with community standards.';
+$subtitle = 'Learn the basics of Nextflow and how to write your own Nextflow pipelines.'
 $markdown_fn = '../markdown/nextflow_tutorial.md';
 include('../includes/header.php');
 include('../includes/footer.php');
