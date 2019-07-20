bdbde615e4cb4e9aafeb99f19b70f6425c98d8e8
Phil Ewels <phil.ewels@scilifelab.se>
2019-07-20 08:03:19 +0200

    Removed orphan PHP files that are no longer needed

--- public_html/usage/introduction.php
+++
@@ -1,7 +0,0 @@
-<?php
-$title = 'Introduction';
-$subtitle = 'Get to grip with the key concepts used in nf-core pipelines.';
-$markdown_fn = '../../markdown/usage/introduction.md';
-include('../../includes/header.php');
-include('../../includes/footer.php');
-?>
