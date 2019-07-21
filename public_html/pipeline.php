3e013fd4ad2f9cca80663b8e22db89c4699f1e3b
Phil Ewels <phil.ewels@scilifelab.se>
2019-07-21 11:54:33 +0200

    Cache pipeline markdown files locally

--- public_html/pipeline.php
+++ public_html/pipeline.php
@@ -11,11 +11,18 @@ if(count($path_parts) > 1){
         header('Location: '.substr($_SERVER['REQUEST_URI'], 0, -3));
         exit;
     }
-    $markdown_fn = 'https://raw.githubusercontent.com/'.$pipeline->full_name.'/master/'.implode('/', array_slice($path_parts, 1)).'.md';
+    $filename = implode('/', array_slice($path_parts, 1)).'.md';
 } else {
-    $markdown_fn = 'https://raw.githubusercontent.com/'.$pipeline->full_name.'/master/README.md';
+    $filename = 'README.md';
     $md_trim_before = '# Introduction';
 }
+$markdown_fn = 'https://raw.githubusercontent.com/'.$pipeline->full_name.'/master/'.$filename;
+if($pipeline->last_release !== 0){
+  $local_md_fn = dirname(dirname(__FILE__))."/markdown/pipelines/".$pipeline->name."/".$pipeline->last_release."/".$filename;
+} else {
+  $local_md_fn = dirname(dirname(__FILE__))."/markdown/pipelines/".$pipeline->name."/".$pipeline->pushed_at."/".$filename;
+}
+
 $src_url_prepend = 'https://raw.githubusercontent.com/'.$pipeline->full_name.'/master/'.implode('/', array_slice($path_parts, 1, -1)).'/';
 $href_url_prepend = $pipeline->name.'/'.implode('/', array_slice($path_parts, 1, -1)).'/';
 $md_content_replace = array(
@@ -26,6 +33,18 @@ $html_content_replace = array(
     array('<table>'),
     array('<table class="table">')
 );
+
+# Check if we have a local copy of the markdown file and fetch if not
+if(file_exists($local_md_fn)){
+  $markdown_fn = $local_md_fn;
+} else {
+  # Build directories if needed
+  if (!is_dir(dirname($local_md_fn))) {
+    mkdir(dirname($local_md_fn), 0777, true);
+  }
+  file_put_contents($local_md_fn, file_get_contents($markdown_fn));
+  $markdown_fn = $local_md_fn;
+}
 
 # Header - keywords
 $header_html = '<p>';
@@ -90,11 +109,11 @@ if(count($pipeline->releases) > 0){
 
 <?php
 
+# echo '<pre>'.print_r($pipeline, true).'</pre>';
+
 # Print rendered markdown made in header.php
 echo '<div class="rendered-markdown">';
 echo $content;
 echo '</div>';
-
-# echo '<pre>'.print_r($pipeline, true).'</pre>';
 
 include('../includes/footer.php');
