14c116ae86bd0f502ebf3145ea1b62bf5a5ca6ca
Phil Ewels <phil.ewels@scilifelab.se>
2019-07-30 16:34:23 +0200

    Fix slack URL on join page

--- public_html/join.php
+++ public_html/join.php
@@ -33,7 +33,7 @@ include('../includes/header.php');
 </p>
 <p class="text-center">
   <a href="https://nf-co.re/join/slack" class="mb-2 btn btn-success">
-    <i class="fab fa-slack"></i> Get Slack invite
+    <i class="fab fa-slack"></i> Join Slack
   </a>
   <a href="https://groups.google.com/forum/#!forum/nf-core" class="mb-2 btn btn-info" style="background-color: #5CA5EF;">
     <i class="fas fa-envelope"></i> Google Groups email list
@@ -52,8 +52,9 @@ include('../includes/header.php');
 </h1>
 <p>Slack is a real-time messaging tool, with discussion split into channels and groups.
 We use it to provide help to people running nf-core pipelines, as well as discussing development ideas.
-You can join the nf-core slack by getting an invite <a href="https://nf-co.re/join/slack">here</a>.</p>
-<p>Once you have registered, you can access the nf-core slack at <a href="https://nf-core.slack.com/">https://nf-core.slack.com/</a></p>
+You can join the nf-core slack <a href="https://nf-co.re/join/slack">here</a>.</p>
+<p>Once you have registered, you can access the nf-core slack at <a href="https://nfcore.slack.com/">https://nfcore.slack.com/</a>
+  <em class="small text-muted">(NB: No hyphen!)</em></p>
 
 <h1>
   <img height="120px" src="/assets/img/github.svg" class="float-right bg-white pl-4" />
