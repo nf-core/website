e9d1692687edb20349075b6712876fce29d6bd0e
Phil Ewels <phil.ewels@scilifelab.se>
2019-07-30 23:23:07 +0200

    Logo: New tool to generate repo logo PNGs

---
+++ public_html/logo.php
@@ -0,0 +1,53 @@
+<?php
+// Let's make a logo!
+
+$textstring = trim(str_replace('logo/', '', $_GET['t']));
+if(!isset($_GET['f'])){
+    $textstring = strtolower($textstring);
+    $textstring = preg_replace("/[^a-z]/", '', $textstring);
+}
+if(strlen($textstring) == 0){
+    header('HTTP/1.1 404 Not Found');
+    include('404.php');
+    die();
+}
+
+// Set the content-type
+header("Content-type: image/png");
+
+// Load the base image
+$image = imagecreatefrompng("assets/img/logo/nf-core-repologo-base.png");
+
+// Keep PNG transparency
+imageAlphaBlending($image, true);
+imageSaveAlpha($image, true);
+
+// Create some colors
+$black = imagecolorallocate($image, 0, 0, 0);
+$font_size = 300;
+$font_path = "../includes/Maven_Pro/MavenPro-Bold.ttf";
+
+// Put text into image
+imagettftext(
+    $image,      // image
+    $font_size,  // size
+    0,           // angle
+    110,         // x
+    850,         // y
+    $black,      // colour
+    $font_path,  // font
+    $textstring  // text
+);
+
+// Crop off the excessive whitespace
+$text_bbox = imagettfbbox($font_size, 0, $font_path, $textstring);
+$text_width = abs($text_bbox[4] - $text_bbox[0]) + 250;
+$min_width = 2300;
+$cropped_image = imagecrop($image, ['x' => 0, 'y' => 0, 'width' => max($text_width, $min_width), 'height' => 1000]);
+imageAlphaBlending($cropped_image, true);
+imageSaveAlpha($cropped_image, true);
+
+// Make and destroy image
+imagepng($cropped_image);
+imagedestroy($cropped_image);
+imagedestroy($image);
