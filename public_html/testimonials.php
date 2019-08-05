e9decb7f4484b03ddaeadf08ab0da4550e19e696
Alexander Peltzer <alex.peltzer@gmail.com>
2019-08-05 11:10:56 +0200

    Better logo, more text

--- public_html/testimonials.php
+++ public_html/testimonials.php
@@ -7,7 +7,8 @@ include('../includes/header.php');
 
 <p>We are collecting statements, projects and other peoples (public) comments on how nf-core contributed to their success in analyzing their data. If you know about something that we should probably list here, please let us know!</p>
 <h1>
+<img height="45px" src="/assets/img/dfg_logo.svg" class="float-right bg-white pl-4" />
   German National Sequencing Initiative
 </h1>
-<p><a href="https://www.dfg.de/en/service/press/press_releases/2018/press_release_no_06/index.html">The German Funding Body (DFG)</a> for establishing four national high-throughput sequencing centers has agreed to rely on nf-core pipelines for analyzing large-scale genomics data generated, with contributors from Kiel and Tübingen already part of the nf-core community and other centers joining soon. </p>
+<p><a href="https://www.dfg.de/en/service/press/press_releases/2018/press_release_no_06/index.html">The German Funding Body (DFG)</a> for establishing four national high-throughput sequencing centers has started to rely on nf-core pipelines for analyzing large-scale genomics data. Contributors from Kiel and Tübingen already actively contributed to the project, the other sequencing centers in Cologne/Bonn (West German Genome Center) and the Dresden Center are in the process of joining and contributing their expertise.</p>
 <?php include('../includes/footer.php');
