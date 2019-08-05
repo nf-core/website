a3f40862239d4e8b72545e4fe8954782fa25a660
Alexander Peltzer <alex.peltzer@gmail.com>
2019-08-05 10:39:06 +0200

    Testimonial section for homepage

---
+++ public_html/testimonials.php
@@ -0,0 +1,13 @@
+<?php
+
+$title = 'nf-core testimonials';
+$subtitle = 'Read about the different people and projects relying on nf-core';
+include('../includes/header.php');
+?>
+
+<p>We are collecting statements, projects and other peoples (public) comments on how nf-core contributed to their success in analyzing their data. If you know about something that we should probably list here, please let us know!</p>
+<h1>
+  German National Sequencing Initiative
+</h1>
+<p><a href="https://www.dfg.de/en/service/press/press_releases/2018/press_release_no_06/index.html">The German Funding Body (DFG)</a> for establishing four national high-throughput sequencing centers has agreed to rely on nf-core pipelines for analyzing large-scale genomics data generated, with contributors from Kiel and Tübingen already part of the nf-core community and other centers joining soon. </p>
+<?php include('../includes/footer.php');
