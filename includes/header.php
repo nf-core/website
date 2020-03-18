4a48c6cbaf49233abbd1c6c7e7559ed43d977de0
matthiasho <matthias.hortenhuber@ki.se>
2020-03-18 16:33:04 +0100

    fix link/script positions for iconpicker in header

--- includes/header.php
+++ includes/header.php
@@ -140,9 +140,6 @@ if(isset($subtitle) && strlen($subtitle) > 0){
     <link href="/assets/css/code_highlighting/github.css" rel="stylesheet" >
     <link href="/assets/css/leaflet.css" rel="stylesheet">
     <link href="/assets/css/Chart.min.css" rel="stylesheet">
-    <link href="/assets/css/iconpicker.css" rel="stylesheet">
-    <link href="/assets/css/nf-core.css?c=<?php echo $git_sha; ?>" rel="stylesheet">
-    <link href="/assets/css/nf-core-<?php echo $theme; ?>.css?c=<?php echo $git_sha; ?>" rel="stylesheet" id="theme-stylesheet">
     <!-- FontAwesome -->
     <script src="https://kit.fontawesome.com/471b59d3f8.js"></script>
     <!-- Global site tag (gtag.js) - Google Analytics -->
@@ -153,6 +150,7 @@ if(isset($subtitle) && strlen($subtitle) > 0){
     <script src="/assets/js/bootstrap.min.js"></script>
     <script src="/assets/js/highlight.pack.js"></script>
     <script src="/assets/js/leaflet.js"></script>
+    <!-- Page-specific CSS and JS -->
     <?php if(isset($import_chartjs) && $import_chartjs): ?>
     <script src="/assets/js/moment.js"></script>
     <script src="/assets/js/Chart.min.js"></script>
@@ -163,14 +161,18 @@ if(isset($subtitle) && strlen($subtitle) > 0){
     <?php endif; ?>
     <?php if(isset($import_schema_builder) && $import_schema_builder): ?>
     <link href="/assets/css/jquery-ui.min.css" rel="stylesheet">
+    <link href="/assets/css/iconpicker.css" rel="stylesheet">
     <script src="/assets/js/jquery-ui.min.js"></script>
     <script src="/assets/js/moment.js"></script>
     <script src="/assets/js/iconpicker.js"></script>
     <script src="/assets/js/nf-core-schema-builder.js?c=<?php echo $git_sha; ?>"></script>
     <?php endif; ?>
     <script src="/assets/js/jquery.tablesorter.min.js"></script>
-    <script src="/assets/js/nf-core.js?c=<?php echo $git_sha; ?>"></script>
 
+    <!-- Custom nf-core CSS and JS -->
+    <link href="/assets/css/nf-core.css?c=<?php echo $git_sha; ?>" rel="stylesheet">
+    <link href="/assets/css/nf-core-<?php echo $theme; ?>.css?c=<?php echo $git_sha; ?>" rel="stylesheet" id="theme-stylesheet">
+    <script src="/assets/js/nf-core.js?c=<?php echo $git_sha; ?>"></script>
     <script>window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);}  gtag('js', new Date()); gtag('config', 'UA-68098153-2'); </script>
   </head>
   <body>
