63fb01d0611525fd12aa2df1e581a3a977fcb594
Phil Ewels <phil.ewels@scilifelab.se>
2020-06-26 17:38:32 +0200

    Building launch GUI

--- public_html/assets/js/nf-core-schema-launcher.js
+++ public_html/assets/js/nf-core-schema-launcher.js
@@ -17,6 +17,7 @@ $(function () {
     });
 
     // Listen to the page scroll
+    var validation_error = false;
     window.onscroll = function(){
         // Progress bar width
         var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
@@ -36,28 +37,52 @@ $(function () {
 
         // Update progress bar
         $('.progress-bar').css('width', scrolled+"%").attr('area-valuenow', scrolled);
-        $('#progress_section').html(newLabel);
+        if(!validation_error){
+            $('#progress_section').html(newLabel);
+        }
     };
 
-    // Parse initial JSON Schema
-    try {
-        schema = JSON.parse($('#json_schema').text());
-    } catch(e){
-        alert("Error - Schema JSON could not be parsed. See the browser console for details.");
-        console.log(e);
-    }
+    // Page-scroll links
+    ///////////////////////////////////////////////////
+    // TODO - NOT WORKING
+    ///////////////////////////////////////////////////
+    $('body').on('.scroll_to_link', 'click', function(e){
+        e.preventDefault();
+        scroll_to($(this));
+    });
 
     // Validate form on submit - snippet from Bootstrap docs
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
     var forms = document.getElementsByClassName('needs-validation');
+    $('#form_validation_error_toast').toast({ 'autohide': false });
     // Loop over them and prevent submission
     var validation = Array.prototype.filter.call(forms, function(form) {
       form.addEventListener('submit', function(event) {
         if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
+            event.preventDefault();
+            event.stopPropagation();
+            validation_error = true;
+            $('.btn-launch').removeClass('btn-primary').addClass('btn-danger');
+            $('.progress-bar').addClass('bg-danger');
+            $('.validation-warning').show();
+            $('#form_validation_error_toast').toast('show');
+            $('#progress_section').html("Error validating inputs");
+            $('#validation_fail_list').html('');
+            $('input:invalid').each(function(){
+                $('#validation_fail_list').append('<li><a href="#'+$(this).attr('id')+'" class="scroll_to_link"><code>'+$(this).attr('name').replace('params_', '--').replace('nxf_flag_', '')+'</code></a></li>')
+            })
+            scroll_to($('input:invalid').first());
         }
         form.classList.add('was-validated');
+
+        // Radio button form group classes
+        $('.form-control:has(.form-check-input[type="radio"]:valid)').addClass('radio-form-control-valid');
+        $('.form-control:has(.form-check-input[type="radio"]:invalid)').addClass('radio-form-control-invalid');
+
+        // Input group addons
+        $('.input-group:has(input:valid) .input-group-prepend, .input-group:has(input:valid) .input-group-append').addClass('input-group-append-valid');
+        $('.input-group:has(input:invalid) .input-group-prepend, .input-group:has(input:invalid) .input-group-append').addClass('input-group-append-invalid');
+
       }, false);
     });
 
