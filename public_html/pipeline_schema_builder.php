ec0e8b5a37fd6608763c663bbc7a6149a0efe52c
Phil Ewels <phil.ewels@scilifelab.se>
2020-07-28 15:45:37 +0200

    Fix settings modal. Testing and more fixes.

--- public_html/pipeline_schema_builder.php
+++ public_html/pipeline_schema_builder.php
@@ -161,7 +161,7 @@ This page helps pipeline authors to build their pipeline schema file by using a 
         <div class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <span class="modal-title h4 text-monospace"></span>
+                    <span class="modal-title h4"></span>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                       <span aria-hidden="true">&times;</span>
                     </button>
