9985f1db664f04befddd7fedd45511c7f966d888
Phil Ewels <phil.ewels@scilifelab.se>
2020-07-28 16:51:24 +0200

    Fix multi_select_modal

--- public_html/pipeline_schema_builder.php
+++ public_html/pipeline_schema_builder.php
@@ -310,7 +310,7 @@ This page helps pipeline authors to build their pipeline schema file by using a 
         <div class="modal-dialog modal-dialog-scrollable modal-lg" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h4>Move parameters into <span></span></h4>
+                    <h4>Move parameters into "<span></span>"</h4>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                       <span aria-hidden="true">&times;</span>
                     </button>
