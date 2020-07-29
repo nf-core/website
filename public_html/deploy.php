41f20519a906b13d7038e1851e4b61f1f2f8124a
Phil Ewels <phil.ewels@scilifelab.se>
2020-07-30 00:04:29 +0200

    New page to show latest version of nf-core/tools
    Fixes nf-core/nf-co.re#436

--- public_html/deploy.php
+++ public_html/deploy.php
@@ -13,8 +13,13 @@ if ( $_POST['payload'] ) {
 
   // Pull the new version of the website repo
   shell_exec("cd /home/nfcore/nf-co.re && git fetch && git reset --hard origin/master >> /home/nfcore/update.log 2>&1 &");
+
   // Pull the new version of the tools repo
   shell_exec("cd /home/nfcore/nf-co.re/includes/nf-core/tools && git fetch && git reset --hard origin/master >> /home/nfcore/update.log 2>&1 &");
+  // Get the latest version number and write to a file
+  shell_exec("cd /home/nfcore/nf-co.re/includes/nf-core/tools && git describe --tags --abbrev=0 > /home/nfcore/nf-co.re/includes/nf-core/tools_version.txt");
+  // Print all version tags to a file
+  shell_exec("cd /home/nfcore/nf-co.re/includes/nf-core/tools && git tag > /home/nfcore/nf-co.re/includes/nf-core/tools_all_versions.txt");
   // Pull the new version of the tools repo (code documentation)
   shell_exec("cd /home/nfcore/tools-docs && git fetch && git reset --hard origin/api-doc >> /home/nfcore/update.log 2>&1 &");
 
