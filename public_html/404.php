0497b6584f41e2f5c3ee7b1b03cd9883e0148c2c
Phil Ewels <phil.ewels@scilifelab.se>
2020-10-10 19:28:12 +0200

    If a 404 GitHub suggestion URL exists, redirect there

--- public_html/404.php
+++ public_html/404.php
@@ -1,4 +1,21 @@
 <?php
+if(isset($suggestion_404_urls)){
+  // Check that suggestion URLs exist
+  foreach($suggestion_404_urls as $idx => $url){
+    $headers = get_headers($url);
+    $http_code = @substr($headers[0], 9, 3);
+    if($http_code != 200 && $http_code !== false){
+      unset($suggestion_404_urls[$idx]);
+    } else {
+      // Redirect to GitHub if is a suggestion URL and exists
+      if(substr($url,0,27) == 'https://github.com/nf-core/'){
+        header("Location: $url");
+        exit();
+      }
+    }
+  }
+}
+
 header('HTTP/1.1 404 Not Found');
 
 $title = 'Error 404';
@@ -15,7 +32,7 @@ include('../includes/header.php');
 <p>Please let us know by <a href="https://github.com/nf-core/nf-co.re" target="_blank">creating an issue on GitHub</a>.</p>
 
 <?php
-if(isset($suggestion_404_urls)){
+if(isset($suggestion_404_urls) && count($suggestion_404_urls) > 0){
   echo '<p>Maybe these are the links that you are looking for?</p><ul>';
   foreach($suggestion_404_urls as $url){
     echo '<li><a href="'.$url.'">'.$url.'</a></li>';
