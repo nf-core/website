f1ee942c93ce8a47760e69d507272e731265deb2
matthiasho <matthias.hortenhuber@ki.se>
2020-10-13 17:10:39 +0200

    add url-copy buttons

--- includes/pipeline_page/example_output.php
+++ includes/pipeline_page/example_output.php
@@ -5,59 +5,72 @@ $mainpage_container = false;
 $aws=true;
 ob_start();
 ?>
-<div id="page-wrapper">
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="row ">
-                <!-- Panel including title, breadcrumbs, and controls -->
-                <div class="col">
-                    <!-- Title and breadcrumbs -->
-                    <div class="d-flex align-items-center">
-                        <!-- App title -->
-                        <div class="title ">
-                            <i class="fab fa-aws fa-lg mr-3 mb-3"></i>
+    <div id="page-wrapper">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="row ">
+                    <!-- Panel including title, breadcrumbs, and controls -->
+                    <div class="col">
+                        <!-- Title and breadcrumbs -->
+                        <div class="d-flex align-items-center">
+                            <!-- App title -->
+                            <div class="title ">
+                                <i class="fab fa-aws fa-lg mr-3 mb-3"></i>
+                            </div>
+                            <!-- Bucket breadcrumbs -->
+                            <div class="">
+                                <ul id="breadcrumb" class="breadcrumb">
+                                    <li class="breadcrumb-item active">
+                                        <a href="#"><i class="fas fa-circle-notch fa-spin"></i></a>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
-                        <!-- Bucket breadcrumbs -->
-                        <div class="">
-                            <ul id="breadcrumb" class="breadcrumb">
-                                <li class="breadcrumb-item active">
-                                    <a href="#"><i class="fas fa-circle-notch fa-spin"></i></a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <!-- Controls -->
-                    <div id="navbuttons" class="">
-                        <div>
-                            <!-- Dual purpose: progress spinner and refresh button, plus object count -->
-                            <div class="btn-group" id="refresh">
-                                <span id="bucket-loader" class="btn fa fa-refresh fa-2x " title="Refresh"></span>
-                                <!-- <span id="badgecount" class="badge pull-right" title="Object count">42</span> -->
+                        <!-- Controls -->
+                        <div id="navbuttons" class="">
+                            <div>
+                                <!-- Dual purpose: progress spinner and refresh button, plus object count -->
+                                <div class="btn-group" id="refresh">
+                                    <span id="bucket-loader" class="btn fa fa-refresh fa-2x " title="Refresh"></span>
+                                    <!-- <span id="badgecount" class="badge pull-right" title="Object count">42</span> -->
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <!-- Panel including S3 object table -->
-            <div class="panel-body">
-                <table class="table table-bordered table-hover responsive" id="tb-s3objects">
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Folder</th>
-                            <th>Last Modified</th>
-                            <th>Size</th>
-                        </tr>
-                    </thead>
-                    <tbody id="tbody-s3objects"></tbody>
-                </table>
+                <!-- Panel including S3 object table -->
+                <div class="panel-body">
+                    <table class="table table-bordered table-hover responsive" id="tb-s3objects">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Folder</th>
+                                <th>Last Modified</th>
+                                <th>Size</th>
+                            </tr>
+                        </thead>
+                        <tbody id="tbody-s3objects"></tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
+    <div id="file-preview" class="card">
+    </div>
 </div>
-<div id="file-preview" class="card">
+<div class="toast" id="url-copied" data-delay="5000" role="alert" aria-live="assertive" aria-atomic="true">
+  <div class="toast-header">
+    <img src="/assets/img/logo/nf-core-logo-square.png" class="rounded mr-2" alt="">
+    <strong class="mr-auto">URL copied to clipboard!</strong>
+    <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+  <!-- <div class="toast-body">
+    Paste this command into your terminal to run the pipeline with a small test dataset.
+  </div> -->
 </div>
-</div>
+
 <script type="text/javascript">
     var HIDE_INDEX = true;
     var s3exp_config = {
