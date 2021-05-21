b102e7fea3d6f916259447f478cb94472ab8cbab
matthiasho <matthias.hortenhuber@ki.se>
2021-05-21 17:13:00 +0200

    add function to highlight current paragraph

--- public_html/assets/js/transcripts.js
+++ public_html/assets/js/transcripts.js
@@ -5,37 +5,64 @@
 
 // Global variables
 var player;
+var timer;
+var current_highlight;
 
     
-      function onYouTubeIframeAPIReady() {
-        player = new YT.Player("video-placeholder", {
-          width: 600,
-          height: 400,
-          videoId: video_id,
-          playerVars: {
-            playsinline: 1,
-            color: "green"
-          },
-          events: {
-            onReady: initialize,
-          },
-        });
-      }
-    function initialize() {
-    //    event.target.playVideo();
-    
-    // player.playVideo();
-    $('details a[href^="https://youtu"]').each(function () {
-      $(this).data("timestamp", $(this.href.split(";t=")).last()[0]);
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player("video-placeholder", {
+    width: 560,
+    height: 315,
+    videoId: video_id,
+    playerVars: {
+      playsinline: 1,
+      color: "green",
+    },
+    events: {
+      onReady: initialize,
+      onStateChange: highlight,
+    },
+  });
+}
+function initialize() {
+$('details a[href^="https://youtu"],details a[href^="https://www.youtu"]').each(function () {
+  let timestamp = $(this.href.split(";t=")).last()[0];
+  if (timestamp.indexOf("s") > 0 || timestamp.indexOf("m") > 0){
+    timestamp =
+      parseInt(timestamp.substring(0, timestamp.indexOf("m")) * 60) +
+      parseInt(
+        timestamp.substring(timestamp.indexOf("m") + 1, timestamp.indexOf("s"))
+      );
+  } else {
+    timestamp = parseInt(timestamp)
+  }
+  $(this).data("timestamp", timestamp);
 
-      if ($(this).data("timestamp")) {
-        $(this).addClass("timestamp-link");
-        this.href = "javascript:void(0)";
+  if ($(this).data("timestamp")) {
+    $(this).addClass("timestamp-link");
+    this.href = "javascript:void(0)";
+  }
+});
+$("a.timestamp-link").click(function (e) {
+  e.preventDefault();
+  player.seekTo($(e.target).data("timestamp"), true);
+});
+}
+function highlight(event) {
+  if (event.data === 1) {
+    timer = setInterval(function () {
+      var current_time = player.getCurrentTime();
+      let all_highlights = $("a.timestamp-link").filter(function () {
+        return $(this).data("timestamp") < current_time;
+      });
+      if (all_highlights.last()!== current_highlight){
+        $(current_highlight).parent("p").removeClass("bg-lightgray");
+        current_highlight = all_highlights.last();
+        $(current_highlight).parent("p").addClass("bg-lightgray");
       }
-    });
-    $("a.timestamp-link").click(function (e) {
-      e.preventDefault();
-      debugger;
-      player.seekTo($(e).data("timestamp"), true);
-    });
-    }
+    }, 1000);
+  } else if(event.data===2) {
+    clearInterval(timer);
+  }
+  
+}
