d64e8bb4c85467d77079afb69c3afb2bc6d3d2c7
matthiasho <matthias.hortenhuber@ki.se>
2021-07-07 19:41:31 +0200

    switch to composer for external libraries

--- public_html/community.php
+++ public_html/community.php
@@ -57,8 +57,10 @@ foreach($logins as $login){
 
 <?php
 // Parse YAML contributors file
+require "../vendor/autoload.php";
+use Spyc;
+
 $locations = [];
-require_once("../includes/libraries/Spyc.php");
 $contributors = spyc_load_file('../nf-core-contributors.yaml');
 $contributors_html = '';
 foreach($contributors['contributors'] as $idx => $c){
