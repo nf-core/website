f5b4768023ab05553eeb36790157581a3dc75f2d
Matthias Hörtenhuber <mashehu3@gmail.com>
2021-12-02 17:01:58 +0100

    fix filename for multiple parameters

--- public_html/logo.php
+++ public_html/logo.php
@@ -30,7 +30,7 @@ if(strlen($textstring) == 0){
 // Check if we have a cached version already
 $cache_filename = $filename;
 if($new_width && is_numeric($new_width)){
-    $cache_filename = 'nfcore-'.preg_replace("/[^a-z]/", '', $textstring).'_logo_w'.$new_width.'.png';
+    $cache_filename = 'nfcore-'.preg_replace("/[^a-z]/", '', $textstring).'_logo_w'.$new_width. '_'. $theme .'.png';
 }
 $logo_cache_fn = dirname(dirname(__FILE__))."/api_cache/logos/{$cache_filename}";
 # Build directories if needed
