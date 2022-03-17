0f494e12c642fc13079e2b8de884198bb65abd76
Francesco <francesco@lescai.eu>
2022-03-17 13:51:59 +0100

    fixed exercise code

--- markdown/usage/tutorials/gitpod_exercises/gitpod_exercise_01.sh
+++ markdown/usage/tutorials/gitpod_exercises/gitpod_exercise_01.sh
@@ -1,7 +1,10 @@
 # Pull a pipeline 
 
-nf-core pull viralrecon
+nextflow pull nf-core/viralrecon
 
 # List available nf-core pipelines and check if the one you pulled is up-to-date
 
 nf-core list
+
+## a colum will show when "Last Pulled"
+## the last column will highlight "Have latest release?"
