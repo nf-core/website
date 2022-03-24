551f3c8c9103dbb80106f4ef60985b8707ac5ceb
Matthias Hörtenhuber <mashehu3@gmail.com>
2022-03-24 15:12:59 +0100

    format document

--- public_html/documentation.php
+++ public_html/documentation.php
@@ -15,19 +15,20 @@ $mainpage_container = false;
 $sidebar_nav_elements = [];
 
 # function to generate an array of the file strcuture
-function dir_tree($dir){
+function dir_tree($dir)
+{
     global $docs_md_base;
     $files = array_map('basename', glob("$dir/*"));
     foreach ($files as $file) {
         if (is_dir("$dir/$file")) {
             $tree[$file] = dir_tree("$dir/$file");
         } else {
-            $fm =file_get_contents("$dir/$file");
+            $fm = file_get_contents("$dir/$file");
             $fm = parse_md_front_matter($fm);
             $tree[] = [
                 'title' => $fm['meta']['title'],
                 'weight' => $fm['meta']['menu']['main']['weight'] ? $fm['meta']['menu']['main']['weight'] : 100,
-                'url' => str_replace($docs_md_base, '', preg_replace('/\.md$/','',"$dir/$file")),
+                'url' => str_replace($docs_md_base, '', preg_replace('/\.md$/', '', "$dir/$file")),
             ];
         }
         # have items with lower weight come first, rest sort by title
@@ -41,15 +42,16 @@ krsort($sidebar_nav_elements, SORT_ASC); # sort Usage before Contributing
 
 # build html for the sidebar nav
 
-function build_sidebar_nav($elements){
+function build_sidebar_nav($elements)
+{
     global $sidebar_nav;
     global $md_fn;
     foreach ($elements as $name => $element) {
-        if(!isset($element['url'])){
+        if (!isset($element['url'])) {
             $path = explode('/', $element[0]['url']);
             $show = strpos($md_fn, implode('/', array_slice($path, 0, -1))) !== false ? 'show' : '';
             $is_open = $show == 'show' ? 'true' : 'false';
-            $id = str_replace(" ", "-", strtolower(implode('-',array_slice($path,-3,2))));
+            $id = str_replace(" ", "-", strtolower(implode('-', array_slice($path, -3, 2))));
             $id = str_replace(":", "", $id);
             $sidebar_nav .= '<button class="btn d-inline-flex align-items-center rounded" data-bs-toggle="collapse" data-bs-target="#' . $id . '" aria-expanded="' . $is_open . '" aria-current="' . $is_open . '">
                             <i class="fas fa-angle-right me-3"></i><strong>' . ucwords($name) . '
@@ -58,7 +60,7 @@ function build_sidebar_nav($elements){
             build_sidebar_nav($element);
             $sidebar_nav .= '</ul></nav>';
         } else {
-            $active = $md_fn == $element['url'].'.md' ? 'active' : '';
+            $active = $md_fn == $element['url'] . '.md' ? 'active' : '';
             $sidebar_nav .= '<li><a href="/' . $element['url'] . '"  class="d-inline-flex align-items-center rounded py-1 px-2 ' . $active . '">' . $element['title'] . '</a></li>';
         }
     }
@@ -71,7 +73,7 @@ include('../includes/header.php');
 
 ?>
 <div class="container-xxl main-content">
-<?php
+    <?php
     $main_content = '<div class="row flex-wrap-reverse flex-lg-wrap">';
 
     # left sidebar
@@ -94,7 +96,7 @@ include('../includes/header.php');
     $main_content .=  '</div>'; # end of the row
 
     echo $main_content;
-?>
+    ?>
 </div>
 
 <?php
