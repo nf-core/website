df9120d913c20ac7ab1054a8ef5d6d9c49f41ccb
Matthias Hörtenhuber <mashehu3@gmail.com>
2022-04-01 14:03:45 +0200

    add back to top on mobile view

--- includes/functions.php
+++ includes/functions.php
@@ -367,6 +367,9 @@ function generate_toc($html_string) {
         $toc .= '</nav>';
         $counter -= 1;
     }
+    $toc_md .= '<a class="dropdown-item" href="#"><i class="fas fa-arrow-to-top"></i> Back to top</a>';
+
+
     $toc_md .= '</div>';
     $toc .= '</div>';
     $toc = $toc_md . $toc;
