c6cbcf77929919c56a62abc0cfabde0bfa68ac31
christopher-hakkaart <chris.hakkaart@seqera.io>
2022-11-02 16:52:43 +0100

    update: general updates

---
+++ public_html/governance.php
@@ -0,0 +1,7 @@
+<?php
+$title = 'Governance';
+$subtitle = 'How nf-core is governed';
+$markdown_fn = '../markdown/governance.md';
+$md_github_url = 'https://github.com/nf-core/nf-co.re/blob/master/markdown/governance.md';
+include '../includes/header.php';
+include '../includes/footer.php';
