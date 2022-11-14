5a10c08f386b06fb9c42605763501218749ccba1
Christopher Hakkaart <chris.hakkaart@seqera.io>
2022-11-14 21:36:24 +0100

    Apply suggestions from code review
    Co-authored-by: Marcel Ribeiro Dantas <mribeirodantas@seqera.io>
    Co-authored-by: Matthias Hörtenhuber <mashehu@users.noreply.github.com>

--- public_html/governance.php
+++ public_html/governance.php
@@ -3,5 +3,6 @@ $title = 'Governance';
 $subtitle = 'How nf-core is governed';
 $markdown_fn = '../markdown/governance.md';
 $md_github_url = 'https://github.com/nf-core/nf-co.re/blob/master/markdown/governance.md';
+$include_mermaid = true;
 include '../includes/header.php';
 include '../includes/footer.php';
