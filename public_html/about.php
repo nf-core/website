b84c2b7dfd688154063c89fed09319fde52fc786
Phil Ewels <phil.ewels@seqera.io>
2022-12-08 10:54:44 +0100

    Delete the PHP website files 🚮

--- public_html/about.php
+++
@@ -1,7 +0,0 @@
-<?php
-$title = 'About nf-core';
-$subtitle = 'Details about the nf-core project - who is involved and how it was started.';
-$markdown_fn = '../markdown/about.md';
-$md_github_url = 'https://github.com/nf-core/nf-co.re/blob/master/markdown/about.md';
-include '../includes/header.php';
-include '../includes/footer.php';
