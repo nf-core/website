1fe92c6a7d5c42b7e714d6f954263ea4b52f08c1
mashehu <mashehu3@gmail.com>
2022-12-13 17:19:56 +0100

    switch to svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
-import { defineConfig } from 'astro/config';
-
 // https://astro.build/config
-import react from '@astrojs/react';
+import svelte from "@astrojs/svelte";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://nf-co.re/',
-    integrations: [react()],
+    integrations: [svelte()],
 });
