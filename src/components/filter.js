d710c7b5ed21ecb53d85fc306cdb99beb0cf6931
mashehu <mashehu3@gmail.com>
2022-12-14 15:49:24 +0100

    feature complete 🎉

--- src/components/filter.js
+++ src/components/filter.js
@@ -4,7 +4,4 @@ import { atom } from 'nanostores';
 export const CurrentFilter = atom([]);
 export const SortBy = atom('');
 export const DisplayStyle = atom('');
-
-CurrentFilter.set(['released', 'under_development', 'archived']);
-SortBy.set('alphabetical');
-DisplayStyle.set('grid');
+export const SearchQuery = atom('');
