cfb268f88fd9a68dd990bc15497eb7b02cc2d2f0
mashehu <mashehu3@gmail.com>
2023-01-06 10:20:39 +0100

    fix import error

--- src/components/EventCard.svelte
+++ src/components/EventCard.svelte
@@ -1,6 +1,9 @@
 <script>
+    // import { ICalendar, GoogleCalendar, OutlookCalendar } from 'datebook'
+    // the above line throws the error: "Named export 'GoogleCalendar' not found." during build, even though it is in the datebook package
+    // so I'm using the following line instead:
     import { ICalendar, GoogleCalendar, OutlookCalendar } from 'datebook'
-    import { saveAs } from 'file-saver'
+    import { saveAs } from 'file-saver';
     export let frontmatter = {
         title: '',
         subtitle: '',
@@ -12,7 +15,6 @@
     };
     export let slug = '';
     export let type_class = '';
-    export let type_icon = '';
     export let time_category = '';
 
     let event_date;
@@ -62,21 +64,21 @@
     } else {
         event_location = frontmatter.location_url[0];
     }
-    const calendar_event  = {
-            title: frontmatter.title,
-            description: frontmatter.subtitle,
-            start: frontmatter.start,
-            end: frontmatter.end,
-            location: event_location}
+    const calendar_event = {
+        title: frontmatter.title,
+        description: frontmatter.subtitle,
+        start: frontmatter.start,
+        end: frontmatter.end,
+        location: event_location,
+    };
 
-    const googleCalendar = new GoogleCalendar(calendar_event).render()
-    const outlookCalendar = new OutlookCalendar(calendar_event).render()
-    let ical = new ICalendar(calendar_event).render()
-    const blob = new Blob([ical], { type: 'text/calendar;charset=utf-8' })
+    const googleCalendar = new GoogleCalendar(calendar_event).render();
+    const outlookCalendar = new OutlookCalendar(calendar_event).render();
+    let ical = new ICalendar(calendar_event).render();
+    const blob = new Blob([ical], { type: 'text/calendar;charset=utf-8' });
     function downloadIcal() {
-        saveAs(blob, frontmatter.title.replace(/[^a-z0-9]/gi, '_').toLowerCase() +'.ics')
+        saveAs(blob, frontmatter.title.replace(/[^a-z0-9]/gi, '_').toLowerCase() + '.ics');
     }
-
 </script>
 
 <div class={'card mb-2 rounded-0 rounded-end border-' + type_class}>
@@ -85,7 +87,7 @@
             <a href={'events/' + slug}>
                 {frontmatter.title}
             </a>
-             {#if time_category === 'current'}
+            {#if time_category === 'current'}
                 <div class="dropwdown btn-group float-end" role="group">
                     <button
                         type="button"
@@ -94,50 +96,57 @@
                         data-bs-toggle="dropdown"
                         aria-haspopup="true"
                         aria-expanded="false"
-                    > Watch now
+                    >
+                        Watch now
                     </button>
                     <div class="dropdown-menu text-secondary">
                         {#if typeof frontmatter.location_url === 'string'}
-                            <a class="dropdown-item" href={frontmatter.location_url} target="_blank" rel="noreferrer"> {frontmatter.location_url} </a>
+                            <a class="dropdown-item" href={frontmatter.location_url} target="_blank" rel="noreferrer">
+                                {frontmatter.location_url}
+                            </a>
                         {:else}
-                        {#each frontmatter.location_url as url}
-                            <a class="dropdown-item" href={url} target="_blank" rel="noreferrer"> {url} </a>
-                        {/each}
+                            {#each frontmatter.location_url as url}
+                                <a class="dropdown-item" href={url} target="_blank" rel="noreferrer"> {url} </a>
+                            {/each}
                         {/if}
                     </div>
                 </div>
-                {/if}
+            {/if}
         </div>
         <div class="card-text">
             <p>{frontmatter.subtitle}</p>
             <div class="d-flex justify-content-between align-items-end">
-            <div>
-                <p class="text-muted">
-                    {event_date}
-                </p>
-            </div>
-            <div class="btn-group" role="group" aria-label="Basic example">
-                <a href={"events/"+slug} class="btn btn-outline-success text-nowrap">See details</a>
-                {#if time_category !== 'past'}
-                <div class="dropwdown btn-group" role="group">
-                    <button
-                        type="button"
-                        class="btn btn-outline-success dropdown-toggle"
-                        href="#"
-                        data-bs-toggle="dropdown"
-                        aria-haspopup="true"
-                        aria-expanded="false"
-                    >
-                        <i class="far fa-calendar-plus me-1" aria-hidden="true" /> Export event
-                    </button>
-                    <div class="dropdown-menu">
-                        <span class="dropdown-item" on:click={()=>downloadIcal()}> Download iCal Event</span>
-                        <a class="dropdown-item" href={googleCalendar} target="_blank"> Add to Google Calendar</a>
-                        <a class="dropdown-item" href={outlookCalendar} target="_blank"> Add to Microsoft Outlook</a>
-                    </div>
+                <div>
+                    <p class="text-muted">
+                        {event_date}
+                    </p>
                 </div>
-                {/if}
-            </div>
+                <div class="btn-group" role="group" aria-label="Basic example">
+                    <a href={'events/' + slug} class="btn btn-outline-success text-nowrap">See details</a>
+                    {#if time_category !== 'past'}
+                        <div class="dropwdown btn-group" role="group">
+                            <button
+                                type="button"
+                                class="btn btn-outline-success dropdown-toggle"
+                                href="#"
+                                data-bs-toggle="dropdown"
+                                aria-haspopup="true"
+                                aria-expanded="false"
+                            >
+                                <i class="far fa-calendar-plus me-1" aria-hidden="true" /> Export event
+                            </button>
+                            <div class="dropdown-menu">
+                                <button class="dropdown-item" on:click={() => downloadIcal()}> Download iCal Event</button>
+                                <a class="dropdown-item" href={googleCalendar} target="_blank"  rel="noreferrer">
+                                    Add to Google Calendar</a
+                                >
+                                <a class="dropdown-item" href={outlookCalendar} target="_blank"  rel="noreferrer">
+                                    Add to Microsoft Outlook</a
+                                >
+                            </div>
+                        </div>
+                    {/if}
+                </div>
             </div>
         </div>
     </div>
