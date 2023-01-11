e7eb35fae256494eb08b9cf6d58d2cf3f4d97df2
mashehu <mashehu3@gmail.com>
2023-01-11 16:23:49 +0100

    add persistent store for event indicator

--- src/components/store.js
+++ src/components/store.js
@@ -1,7 +1,8 @@
+import { persistentAtom } from '@nanostores/persistent';
 import { atom } from 'nanostores';
 
 export const CurrentFilter = atom([]);
 export const SortBy = atom('');
 export const DisplayStyle = atom('');
 export const SearchQuery = atom('');
-export const EventIsOngoing = atom(false);
+export const EventIsOngoing = persistentAtom(false);
