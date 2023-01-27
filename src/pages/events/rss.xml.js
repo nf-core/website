6972014021eb9ff24444051eeee51186091c7062
mashehu <mashehu3@gmail.com>
2023-01-27 11:21:19 +0100

    fix rss feed

--- src/pages/events/rss.xml.js
+++ src/pages/events/rss.xml.js
@@ -1,10 +1,18 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
-export const get = () =>
-  rss({
+export async function get(context) {
+  const events = await getCollection('events');
+  return rss({
     title: 'nf-core: Events',
     description: 'Details of past and future nf-core meetups.',
-    site: import.meta.env.SITE,
+    site: context.site,
     stylesheet: '/rss/styles.xsl',
-    items: pagesGlobToRssItems(import.meta.glob('./events/*.{md,mdx}')),
+    items: events.map((event) => ({
+      title: event.data.title,
+      description: event.data.subtitle,
+      link: '/events/' + event.slug,
+      pubDate: event.data.start,
+    })),
   });
+}
