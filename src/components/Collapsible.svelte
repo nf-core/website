e44279a241f2c0e8d556655dad20556c79626a74
mashehu <mashehu3@gmail.com>
2023-01-30 18:33:06 +0100

    add collapsible component

---
+++ src/components/Collapsible.svelte
@@ -0,0 +1,25 @@
+<script>
+  import {showHelp} from './store.js'
+  export let buttonText = "help text";
+  export let buttonClass = "btn-outline-info me-auto";
+  export let textClass = "";
+  $: show = $showHelp;
+</script>
+
+<div>
+  <div class="">
+    <button class={"btn " + buttonClass} class:open={show} type="button" on:click={() => (show = !show)}>
+      {buttonText}
+    </button>
+  </div>
+  <div class={"collapse " + textClass} class:show>
+    <slot />
+  </div>
+</div>
+
+<style lang="scss">
+  .btn.open {
+    border-bottom-left-radius: 0;
+    border-bottom-right-radius: 0;
+  }
+</style>
