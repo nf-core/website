f505ae2fad4299e4a11e120462d605925977ad4d
mashehu <mashehu3@gmail.com>
2023-01-30 18:30:27 +0100

    add parameter page per pipeline

---
+++ src/components/SchemaListing.svelte
@@ -0,0 +1,16 @@
+<script>
+  import SchemaListingGroup from "./SchemaListingGroup.svelte";
+
+  export let schema;
+  // console.log(schema.definitions);
+</script>
+
+<div class="schema-listing">
+  {#if schema.definitions}
+    <div class="d-flex flex-column">
+      {#each Object.entries(schema.definitions) as [id, definition] (id)}
+        <SchemaListingGroup {definition} {id}/>
+      {/each}
+    </div>
+  {/if}
+</div>
---
+++ src/components/SchemaListingToc.svelte
@@ -0,0 +1,23 @@
+
+<script>
+  import {showHidden,showHelp} from './store.js'
+
+  export let schema;
+  // console.log(schema.definitions);
+</script>
+
+<div class="toc">
+  <h2>On this page</h2>
+  <button type="button" class="btn btn-outline-secondary" on:click={()=>showHidden.set(!$showHidden)}>Show hidden</button>
+  <button type="button" class="btn btn-outline-secondary" on:click={()=>showHelp.set(!$showHelp)}>Show all help</button>
+  
+  <!-- <p  class="text-success">{schema.description}</p>
+  {#if schema.definitions}
+    <div class="d-flex flex-column">
+
+      {#each Object.entries(schema.definitions) as [_, definition]}
+        
+      {/each}
+    </div>
+  {/if} -->
+</div>
