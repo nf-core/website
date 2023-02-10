54d5ea15427f3a9a027c63310755ae1daae2367f
mashehu <mashehu3@gmail.com>
2023-02-10 17:15:29 +0100

    render all pipeline docs

--- src/components/SidebarToc.svelte
+++ src/components/SidebarToc.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import { currentHeading } from '@components/store';
+
     export let headings: {
         text: string;
         slug: string;
@@ -6,28 +8,26 @@
         fa_icon?: string;
     }[];
 
-    // get minimal heading depth from headings
-    let max_heading_depth = 3;
-    // filter out headings that are higher than max_heading_depth
-    headings = headings.filter((h) => h.depth <= max_heading_depth);
-
-    let min_heading_depth = Math.min(...headings.map((h) => h.depth));
+    const min_heading_depth = Math.min(...headings.map((h) => h.depth));
     // make margin classes from min to max heading depth
     let headingMargin = {};
-    for (let i = min_heading_depth; i <= 6; i++) {
+    for (let i = min_heading_depth; i <= 4; i++) {
         headingMargin[i] = 'ps-' + (i - min_heading_depth) * 2;
     }
 </script>
 
 <div class="nav flex-column sticky-top-under">
-    <div class="d-none d-md-block">
+    <div class="d-none d-md-inline">
         <strong class="h6 my-2 text-body">On this page</strong>
-        <hr class="my-1" />
-        <nav id="TableOfContents d-none d-md-flex flex-column ">
-            <ul class="mb-0">
+        <!-- <hr class="my-1" /> -->
+        <nav id="TableOfContents d-none d-md-flex flex-column">
+            <ul class="mb-0 mt-1">
                 {#each headings as heading (heading)}
-                    <li class={'nav-item ' + headingMargin[heading.depth]} class:active={heading.depth==2}>
-                        <a class="nav-link py-1 ps-1 text-muted" href={'#' + heading.slug}>
+                    <li
+                        class={'nav-item ' + headingMargin[heading.depth]}
+                        class:active={heading.slug === $currentHeading}
+                    >
+                        <a class="nav-link py-1 ps-3 text-muted" href={'#' + heading.slug}>
                             {#if heading.fa_icon}
                                 <i class={heading.fa_icon} aria-hidden="true" />
                             {/if}
@@ -60,14 +60,15 @@
         overflow-y: auto;
         max-height: calc(100% - 56rem);
     }
-    .sticky-top-under{
+    .sticky-top-under {
         top: 5.05rem;
         position: sticky;
     }
-    li{
-        border-left: 2pt solid $secondary;
+    li {
+        border-inline-start: 2pt solid $border-color-dark;
+        transition: background-color 0.3s ease-out, border-left 0.3s ease-out;
     }
-    li.active{
+    li.active {
         border-left: 2pt solid $success;
         background-color: transparentize($success, 0.75);
     }
--- src/components/store.js
+++ src/components/store.js
@@ -1,13 +1,14 @@
 import { persistentAtom } from '@nanostores/persistent';
 import { atom } from 'nanostores';
 
+
 export const CurrentFilter = atom([]);
 export const SortBy = atom('');
 export const DisplayStyle = atom('');
 export const SearchQuery = atom('');
 export const showHidden = atom(false);
 export const showHelp = atom(false);
-
+export const currentHeading = atom('');
 export const EventIsOngoing = persistentAtom('EventIsOngoing', false, {
   encode(value) {
     return JSON.stringify(value);
