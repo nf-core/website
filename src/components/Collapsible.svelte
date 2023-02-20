41bc63cd0140de991a5db848713ffb6cc6519a04
mashehu <mashehu3@gmail.com>
2023-02-20 15:37:40 +0100

    params page: move help text button, make table wider

--- src/components/Collapsible.svelte
+++ src/components/Collapsible.svelte
@@ -1,25 +1,25 @@
-<script>
-  import {showHelp} from './store.js'
-  export let buttonText = "help text";
-  export let buttonClass = "btn-outline-info me-auto";
-  export let textClass = "";
-  $: show = $showHelp;
+<script lang="ts">
+    import { showHelp } from './store.js';
+    export let buttonText: string = 'Help text';
+    export let buttonClass: string = 'btn-outline-secondary ms-auto';
+    export let textClass: string = '';
+    $: show = $showHelp;
 </script>
 
 <div>
-  <div class="">
-    <button class={"btn " + buttonClass} class:open={show} type="button" on:click={() => (show = !show)}>
-      {buttonText}
-    </button>
-  </div>
-  <div class={"collapse " + textClass} class:show>
-    <slot />
-  </div>
+    <div class="d-flex">
+        <button class={'btn ' + buttonClass} class:open={show} type="button" on:click={() => (show = !show)}>
+            {buttonText}
+        </button>
+    </div>
+    <div class={'collapse ' + textClass} class:show>
+        <slot />
+    </div>
 </div>
 
 <style lang="scss">
-  .btn.open {
-    border-bottom-left-radius: 0;
-    border-bottom-right-radius: 0;
-  }
+    .btn.open {
+        border-bottom-left-radius: 0;
+        border-bottom-right-radius: 0;
+    }
 </style>
