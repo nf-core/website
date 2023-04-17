cf16acd0c2012cc95dea1ef8427dc93a309e2e5e
mashehu <mashehu3@gmail.com>
2023-04-17 14:11:40 +0200

    use svelte markdown element to render description and help texts as markdown on the parameters page

--- src/components/SchemaListingElement.svelte
+++ src/components/SchemaListingElement.svelte
@@ -1,17 +1,11 @@
 <script>
     import { showHidden } from './store.js';
-    // import { markdown } from '@astropub/md';
     import Collapsible from './Collapsible.svelte';
     import Markdown from './Markdown.svelte';
 
     export let title;
     export let property;
     const id = title.replace(' ', '-');
-
-    // let help_text = '';
-    // if (property.help_text) {
-    //     help_text = markdown(property.help_text);
-    // }
 </script>
 
 <div
@@ -21,12 +15,12 @@
 >
     <div class="col-12 col-md-3 col-xl-2 title border-right border-secondary overflow-x-scroll text-nowrap">
         {#if property.fa_icon}
-            <i class="fa fa-fw me-2 {property.fa_icon}" />
+            <i class="fa fa-fw {property.fa_icon}" />
         {/if}
         <code>--{title}</code>
     </div>
     <div class="col description">
-        {property.description}
+        <Markdown md={property.description} />
     </div>
     <div class="col-12 col-md-3 col-xl-2 text-nowrap d-flex flex-column align-items-end justify-content-between">
         {#if property.hidden}
@@ -52,9 +46,7 @@
         <div class="row d-flex mt-2 mx-0 w-100 px-0 gx-3 gx-md-4">
             <Collapsible>
                 <div {id} class="p-2 px-3 text-body bg-secondary-subtle border border-secondary rounded-3">
-                    {property.help_text}
                     <Markdown md={property.help_text} />
-                    <!-- <Markdown of={property.help_text} /> -->
                 </div>
             </Collapsible>
         </div>
