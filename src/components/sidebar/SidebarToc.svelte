3714e973b752a303febfe7630281179fe8a1c9d4
mashehu <mashehu3@gmail.com>
2023-05-23 10:13:44 +0200

    polish pipeline components

--- src/components/sidebar/SidebarToc.svelte
+++ src/components/sidebar/SidebarToc.svelte
@@ -23,40 +23,42 @@
     });
 </script>
 
-<div class="nav flex-column sticky-top-under align-items-end">
-    <div class="d-none d-md-inline">
-        <strong class="h6 my-2 text-body">On this page</strong>
-        <!-- <hr class="my-1" /> -->
-        <nav id="TableOfContents" class="d-none d-md-flex flex-column">
-            <ul class="mb-0 mt-1">
-                {#each headings as heading (heading)}
-                    <li
-                        class={'nav-item ' + headingMargin[heading.depth]}
-                        class:active={heading.slug === $currentHeading}
+{#if headings.length > 0}
+    <div class="nav flex-column sticky-top-under align-items-end">
+        <div class="d-none d-md-inline">
+            <strong class="h6 my-2 text-body">On this page</strong>
+            <!-- <hr class="my-1" /> -->
+            <nav id="TableOfContents" class="d-none d-md-flex flex-column">
+                <ul class="mb-0 mt-1">
+                    {#each headings as heading (heading)}
+                        <li
+                            class={'nav-item ' + headingMargin[heading.depth]}
+                            class:active={heading.slug === $currentHeading}
+                        >
+                            <a class="nav-link py-1 ps-3 text-muted" href={'#' + heading.slug}>
+                                {#if heading.fa_icon}
+                                    <i class={heading.fa_icon} aria-hidden="true" />
+                                {/if}
+                                {@html heading.text}
+                            </a>
+                        </li>
+                    {/each}
+                </ul>
+                <div class="">
+                    <a
+                        href="#/"
+                        class="back-to-top text-muted text-small float-end mb-2"
+                        on:click={() => window.scrollTo(0, 0)}
                     >
-                        <a class="nav-link py-1 ps-3 text-muted" href={'#' + heading.slug}>
-                            {#if heading.fa_icon}
-                                <i class={heading.fa_icon} aria-hidden="true" />
-                            {/if}
-                            {@html heading.text}
-                        </a>
-                    </li>
-                {/each}
-            </ul>
-            <div class="">
-                <a
-                    href="#/"
-                    class="back-to-top text-muted text-small float-end mb-2"
-                    on:click={() => window.scrollTo(0, 0)}
-                >
-                    <i class="fa-solid fa-arrow-up-to-line" aria-hidden="true" /> Back to top
-                </a>
+                        <i class="fa-solid fa-arrow-up-to-line" aria-hidden="true" /> Back to top
+                    </a>
 
-                <slot />
-            </div>
-        </nav>
+                    <slot />
+                </div>
+            </nav>
+        </div>
     </div>
-</div>
+{/if}
 
 <style lang="scss">
     @import 'src/styles/_variables.scss';
