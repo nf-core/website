e7355b4ddc728016b84f38f095986e18053ce5b2
mashehu <mashehu3@gmail.com>
2023-06-09 14:55:29 +0200

    switch to better fuzzyfinder

---
+++ src/components/FuzzySearch.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    import fuzzysort from 'fuzzysort';
+    import { onMount } from 'svelte';
+
+    export let possibleResults: { href: string; name: string }[] = [];
+    export let placeholder: string = 'Search';
+
+    let results = [];
+
+    function search(event: KeyboardEvent) {
+        const input = event.target as HTMLInputElement;
+        const value = input.value;
+        if (value) {
+            // search for the value
+            results = fuzzysort.go(value, possibleResults, {
+                key: 'name',
+            });
+        } else {
+            results = [];
+        }
+    }
+</script>
+
+<div>
+    <input class="form-control form-control" type="text" {placeholder} aria-label={placeholder} on:keyup={search} />
+    <div class="search-results dropdown-menu" class:show={results.length > 0}>
+        {#if results}
+            {#each results as result (result)}
+                <li>
+                    <a href={result.obj.href} id={result.obj.name} class="text-decoration-none text-body">
+                        {@html fuzzysort.highlight(result, '<span class="text-bg-success">', '</span>')}
+                    </a>
+                </li>
+            {/each}
+        {/if}
+    </div>
+</div>
