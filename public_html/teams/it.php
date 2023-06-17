4ad254761bc4d822024f49cf4bb5b60d5c138faa
Priyanka Surana <ps22@sanger.ac.uk>
2023-06-17 15:25:58 +0100

    Create it.php

---
+++ public_html/teams/it.php
@@ -0,0 +1,7 @@
+<?php
+$title = 'Informatics Infrastructure (IT) team';
+$subtitle = 'Support the production of reference genomes, deployment of large-scale analyses, and management of informatics resources.';
+$markdown_fn = '../markdown/teams/informatics-infrastructure.md';
+$md_github_url = 'https://github.com/sanger-tol/pipelines-website/blob/master/markdown/teams/informatics-infrastructure.md';
+include '../includes/header.php';
+include '../includes/footer.php';
