3328347ff1c06eacc7deb24b6abf1b986db84f9b
mashehu <mashehu3@gmail.com>
2023-06-20 17:47:54 +0200

    add buttons to file preview and add pdf support

---
+++ src/components/CopyButton.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    export let text: string;
+    $: copied = false;
+    const copyToClipboard = (text: string) => {
+        navigator.clipboard.writeText(text);
+
+        copied = true;
+
+        setTimeout(() => {
+            copied = false;
+        }, 1000);
+        return true;
+    };
+</script>
+
+<span class="copy-url" on:click={() => copyToClipboard(text)} on:keypress={() => copyToClipboard(text)}>
+    <slot />
+    {#if copied}
+        <i class="fa-regular px-1 fa-clipboard-check" />
+    {/if}
+</span>
