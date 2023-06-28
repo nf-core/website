6e0bc3f59a9d26adc4f2aac142c64bbae8fd700f
Priyanka Surana <ps22@sanger.ac.uk>
2023-06-28 13:25:53 +0100

    Update code_of_conduct.php

--- public_html/code_of_conduct.php
+++ public_html/code_of_conduct.php
@@ -1,7 +1,7 @@
 <?php
 $title = 'Code of conduct';
-$subtitle = 'The nf-core code of conduct';
+$subtitle = 'The sanger-tol code of conduct';
 $markdown_fn = '../CODE_OF_CONDUCT.md';
-$md_github_url = 'https://github.com/nf-core/nf-co.re/blob/master/CODE_OF_CONDUCT.md';
+$md_github_url = 'https://github.com/sanger-tol/pipelines-website/blob/main/CODE_OF_CONDUCT.md';
 include '../includes/header.php';
 include '../includes/footer.php';
