dbf0276a247c8231793d3af87b9e4c0282821e35
mashehu <mashehu3@gmail.com>
2023-07-03 10:58:17 +0200

    fix date handling in firefox and safari

--- src/components/event/EventBannerElement.svelte
+++ src/components/event/EventBannerElement.svelte
@@ -18,12 +18,6 @@
             if (event.data.title.toLowerCase().match('bytesize')) {
                 event.data.type = 'bytesize';
             }
-            event.data.start = new Date(event.data.start_date + ' ' + event.data.start_time);
-            event.data.end = new Date(event.data.end_date + ' ' + event.data.end_time);
-            console.log('event_start', new Date(event.data.start_date));
-            console.log('event_end', new Date(event.data.end_date));
-            console.log('event.data.start', new Date(event.data.start_date + ' ' + event.data.start_time));
-            console.log('event.data.end', new Date(event.data.end_date + ' ' + event.data.end_time));
             if (event.data.start_date === event.data.end_date) {
                 event.data.duration =
                     event.data.start.toLocaleString('en-US', {
