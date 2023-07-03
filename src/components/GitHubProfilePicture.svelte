da2439914688e0a71c7cc156adafdc3cc82d9317
mashehu <mashehu3@gmail.com>
2023-07-03 15:28:11 +0200

    make github avatars link

--- src/components/GitHubProfilePicture.svelte
+++ src/components/GitHubProfilePicture.svelte
@@ -9,16 +9,18 @@
     const avatar_url = image.match(/\?/) ? `${image}&s=${size}` : `${image}?s=${size}`;
 </script>
 
-<img
-    src={avatar_url}
-    class="me-1 mb-1"
-    class:rounded-circle={circle}
-    data-bs-placement="bottom"
-    data-bs-toggle="tooltip"
-    title={tooltip}
-    alt={`Github user ${name}`}
-    style="--size:{size};"
-/>
+<a href="https://github.com/{name}" class="text-decoration-none" target="_blank" rel="noopener noreferrer">
+    <img
+        src={avatar_url}
+        class="me-1 mb-1"
+        class:rounded-circle={circle}
+        data-bs-placement="bottom"
+        data-bs-toggle="tooltip"
+        title={tooltip}
+        alt={`Github user ${name}`}
+        style="--size:{size};"
+    />
+</a>
 
 <style lang="scss">
     img {
