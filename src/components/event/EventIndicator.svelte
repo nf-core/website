4d4ae343fb97b6a854f75e50448f0c38780d09cc
mashehu <mashehu3@gmail.com>
2023-07-03 10:32:53 +0200

    make sidebar sections links

--- src/components/event/EventIndicator.svelte
+++ src/components/event/EventIndicator.svelte
@@ -10,12 +10,12 @@
 
 <style lang="scss">
     @import '@styles/_variables.scss';
-    .event-indicator :global([astro-icon]) {
+    .event-indicator :global(svg[astro-icon]) {
         fill: $danger;
         width: 1rem;
         margin-bottom: 0.1rem;
     }
-    .event-indicator :global([astro-icon] .fa-secondary) {
+    .event-indicator :global(svg[astro-icon] .fa-secondary) {
         opacity: 0.4;
     }
 </style>
