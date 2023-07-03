ea9552703e09f7a968eabb0c36dc1a70add69047
mashehu <mashehu3@gmail.com>
2023-07-03 08:55:16 +0200

    workaround for weird variable handling in videobutton.svelte

--- src/components/event/EventBannerElement.svelte
+++ src/components/event/EventBannerElement.svelte
@@ -1,26 +1,26 @@
-<script>
+<script lang="ts">
     import { EventIsOngoing } from '@components/store';
     import { formatDistanceToNow } from 'date-fns';
     import ExportEventButton from '@components/event/ExportEventButton.svelte';
     import VideoButton from '@components/VideoButton.svelte';
     export let events = [];
-    export let event_time_category = '';
+    export let event_time_category: ['ongoing', 'upcoming'];
 
     export let event_type_classes = {};
     export let event_type_icons = {};
 
     let backgroundIcon = '';
-
+    console.log('events', events);
     if (event_time_category === 'upcoming') {
         backgroundIcon = 'fa-alarm-clock';
         events = events.filter((event) => {
-            let time_window = 70 * 24 * 60 * 60 * 1000; //TODO: change back to 1 day
+            let time_window = 1 * 24 * 60 * 60 * 1000; //TODO: change back to 1 day
             const event_start_unix = event.data.start.getTime();
             const event_end_unix = event.data.end.getTime();
 
             // increase time window to a week for events longer than 5 hours
             if (event_end_unix - event_start_unix > 5 * 60 * 60 * 1000) {
-                time_window = 70 * 24 * 60 * 60 * 1000; //TODO: change back to 7 days
+                time_window = 7 * 24 * 60 * 60 * 1000; //TODO: change back to 7 days
             }
             if (event.data.start < new Date() && new Date() < event.data.end) {
                 return false;
@@ -63,9 +63,9 @@
             if (event.data.title.toLowerCase().match('bytesize')) {
                 event.data.type = 'bytesize';
             }
-            console.log(event);
             event.data.start = new Date(event.data.start_date + ' ' + event.data.start_time);
             event.data.end = new Date(event.data.end_date + ' ' + event.data.end_time);
+            console.log(event);
             if (event.data.start_date === event.data.end_date) {
                 event.data.duration =
                     event.data.start.toLocaleString('en-US', {
