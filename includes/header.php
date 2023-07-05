7ca3a12bdbb72291cfa035dc0803ec80ebaadbe2
Priyanka Surana <ps22@sanger.ac.uk>
2023-07-05 10:07:47 +0100

    Add modules

--- includes/header.php
+++ includes/header.php
@@ -162,6 +162,10 @@ if (isset($subtitle) && strlen($subtitle) > 0) {
           <li class="nav-item p-1">
             <a class="nav-link" href="/pipelines">Pipelines</a>
           </li>
+
+          <li class="nav-item p-1">
+            <a class="nav-link" href="/modules">Modules</a>
+          </li>
             
           <li class="nav-item p-1">
             <a class="nav-link" href="/tools/">Tools</a>
