e3cc1110bf3bf7f3245909bf981012b557afe760
mashehu <mashehu3@gmail.com>
2023-07-07 10:42:46 +0200

    slim down index and 404, add static stats page,

--- public_html/404.php
+++ public_html/404.php
@@ -25,23 +25,13 @@ if ($_SERVER['REQUEST_URI'] != '/404') {
     $protocol = isset($_SERVER['HTTPS']) && !empty($_SERVER['HTTPS']) ? 'https://' : 'http://';
     $request_url = '<code>' . $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . '</code>';
 }
-include '../includes/header.php';
+include '../includes/slim_header.php';
 ?>
 <h1>Sorry, that page could not be found.</h1>
-<p>It looks like <?php echo $request_url; ?> doesn't exist. </p>
-<p>Please let us know by <a href="https://github.com/nf-core/nf-co.re" target="_blank">creating an issue on GitHub</a>.</p>
+<p>It looks like <?php echo $request_url; ?> doesn't exist.</p>
+<p>Please be aware that this is the place of the old nf-co.re website code. For the current pages, go to <a href="https://nf-co.re/">nf-co.re</a>.</p>
+
 
 <?php
-if (isset($suggestion_404_urls) && count($suggestion_404_urls) > 0) {
-    echo '<p>Maybe these are the links that you are looking for?</p><ul>';
-    foreach ($suggestion_404_urls as $url) {
-        echo '<li><a href="' . $url . '">' . $url . '</a></li>';
-    }
-    echo '</ul>';
-    echo '<img src="/assets/img/thesearenotthedroidsyourelookingfor.gif" class="w-50">';
-}
-
-include '../includes/footer.php';
-
 
 ?>
---
+++ public_html/index.php
@@ -0,0 +1,4 @@
+<?php
+echo "This is the cemetery of what was once the PHP code for the nf-core website. The current site is at nf-co.re.Nothing to see here except redirects to specific pages we haven't ported yet.";
+
+?>
