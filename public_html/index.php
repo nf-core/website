51e15f72fd7905f5e92ef939d3c8038f4abebba6
Matthias Hörtenhuber <mashehu@users.noreply.github.com>
2023-07-07 11:13:32 +0200

    Update public_html/index.php
    Co-authored-by: Phil Ewels <phil.ewels@seqera.io>

--- public_html/index.php
+++ public_html/index.php
@@ -1,4 +1,1 @@
-<?php
-echo "This is the cemetery of what was once the PHP code for the nf-core website. The current site is at nf-co.re.Nothing to see here except redirects to specific pages we haven't ported yet.";
-
-?>
+This is the cemetery of what was once the PHP code for the nf-core website. The current site is at https://nf-co.re - nothing to see here except redirects to specific pages we haven't ported yet.
