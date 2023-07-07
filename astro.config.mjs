d45779035e064c24486f8e98e7f91a71187434f1
mashehu <mashehu3@gmail.com>
2023-07-07 08:27:48 +0200

    improve font rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -56,6 +56,7 @@ export default defineConfig({
             FontaineTransform.vite({
                 // avoid flash of unstyled text by interjecting fallback system fonts https://developer.chrome.com/blog/framework-tools-font-fallback/#using-fontaine-library
                 fallbacks: ['BlinkMacSystemFont', 'Segoe UI', 'Helvetica Neue', 'Arial', 'Noto Sans'],
+                resolvePath: (id) => new URL(`./public${id}`, import.meta.url),
             }),
         ],
         ssr: {
