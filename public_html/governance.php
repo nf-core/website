d8f5e3038098b451b50b9ff664bf6b8b2887b91c
Guoying Qi <gq2@sanger.ac.uk>
2023-07-12 12:47:28 +0100

    remove the governance and and php files

--- public_html/governance.php
+++
@@ -1,8 +0,0 @@
-<?php
-$title = 'Governance';
-$subtitle = 'How nf-core is governed';
-$markdown_fn = '../markdown/governance.md';
-$md_github_url = 'https://github.com/nf-core/nf-co.re/blob/master/markdown/governance.md';
-$include_mermaid = true;
-include '../includes/header.php';
-include '../includes/footer.php';
