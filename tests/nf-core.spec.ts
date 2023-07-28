ab0fe2a642a79eb20a200c12e1639aebc2d7072c
mashehu <mashehu3@gmail.com>
2023-07-28 11:03:08 +0200

    include suggestions from @alvaromartmart

--- tests/nf-core.spec.ts
+++ tests/nf-core.spec.ts
@@ -6,26 +6,26 @@ import { test, expect } from '@playwright/test';
 test('meta is correct', async ({ page }) => {
     await page.goto('/');
 
-    await expect(page).toHaveTitle('nf-core');
+    await expect.soft(page).toHaveTitle('nf-core');
 });
 
 test('pipeline redirect works for /$pipeliname', async ({ page }) => {
     await page.goto('/rnaseq');
-    await expect(page).toHaveTitle('nf-core/rnaseq');
+    await expect.soft(page).toHaveTitle('nf-core/rnaseq');
     // check if markdown is rendered correctly
-    await expect(page.locator('.markdown-content')).toContainText('nf-core/rnaseq is a bioinformatics pipeline');
+    await expect.soft(page.locator('.markdown-content')).toContainText('nf-core/rnaseq is a bioinformatics pipeline');
     // check if results redirect works
     await page.goto('/rnaseq/results/');
 
-    await expect(page.getByRole('link', { name: 'Results' })).toHaveClass('nav-link active');
+    await expect.soft(page.getByRole('link', { name: 'Results' })).toHaveClass('nav-link active');
     // check if SSR works correctly for results
     await page.locator('.list-group-item').nth(1).click();
-    await expect(page.locator('.file-browser')).toContainText('fastqc');
+    await expect.soft(page.locator('.file-browser')).toContainText('fastqc');
 });
 
 test('random pipeline page', async ({ page }) => {
     await page.goto('/');
     await page.getByRole('link', { name: 'View Pipelines' }).click();
     await page.locator('.card').locator('a').first().click();
-    await expect(page.locator('.markdown-content')).toContainText('Citations');
+    await expect.soft(page.locator('.markdown-content')).toContainText('Citations');
 });
