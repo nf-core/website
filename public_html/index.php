298fc05baa4087fb3218534d7d35b784f856cc7c
Guoying Qi <gq2@sanger.ac.uk>
2023-08-14 14:11:23 +0100

    comments the variables not being used.

--- public_html/index.php
+++ public_html/index.php
@@ -29,8 +29,8 @@ shuffle($contributors_img_list);
 
 //Check if there is an event
 // these 2 variables not being used any more, but update them to sanger-tol now
-$md_github_url = 'https://github.com/sanger-tol/pipelines-website/tree/main/markdown/events';
-$header_btn_url = 'https://pipelines.tol.sanger.ac.uk/events/rss';
+// $md_github_url = 'https://github.com/sanger-tol/pipelines-website/tree/main/markdown/events';
+// $header_btn_url = 'https://pipelines.tol.sanger.ac.uk/events/rss';
 
 # To get parse_md_front_matter() and sanitise_date_meta() functions
 require_once '../includes/functions.php';
