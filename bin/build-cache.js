e8b590500a06463b8abfba9f6e9f78a507d65ddc
mashehu <mashehu3@gmail.com>
2023-09-19 08:30:12 +0200

    trying to avoid secondary API limit, update cache handling for playwright

--- bin/build-cache.js
+++ bin/build-cache.js
@@ -5,6 +5,7 @@ import { readFileSync } from 'fs';
 import path from 'path';
 import ProgressBar from 'progress';
 
+
 const cache = Cache.default({
   basePath: './.cache',
   ns: 'nf-core',
@@ -35,21 +36,20 @@ export const buildCache = async () => {
         release.doc_files.push('nextflow_schema.json'); // add the nextflow_schema.json to the cache
       }
       const version = release.tag_name;
-      await Promise.all(
-        release.doc_files.map(async (f) => {
-          const cache_key = `${name}/${version}/${f}`;
-          // console.log(`Checking ${cache_key}`);
-          const is_cached = cache.getSync(cache_key, false) && cache.getSync(cache_key, false).length > 0;
-          if (!is_cached || force || version === 'dev') {
-            const content = await getGitHubFile(name, f, version);
-            // console.log(`Caching ${cache_key}`);
-            cache.set(cache_key, content);
-            // console.log(`Cached ${cache_key}`);
-          } else {
-            // console.log(`Already cached ${cache_key}`);
-          }
-        }),
-      );
+
+      release.doc_files.map(async (f) => {
+        const cache_key = `${name}/${version}/${f}`;
+        // console.log(`Checking ${cache_key}`);
+        const is_cached = cache.getSync(cache_key, false) && cache.getSync(cache_key, false).length > 0;
+        if (!is_cached || force || version === 'dev') {
+          const content = await getGitHubFile(name, f, version);
+          // console.log(`Caching ${cache_key}`);
+          cache.set(cache_key, content);
+          // console.log(`Cached ${cache_key}`);
+        } else {
+          // console.log(`Already cached ${cache_key}`);
+        }
+      });
     }
 
     bar.tick();
