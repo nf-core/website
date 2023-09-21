8292e05e416009ad255c237ad9b8d010a8f27675
mashehu <mashehu3@gmail.com>
2023-09-21 10:26:14 +0200

    format date with svelte

--- src/components/event/LocalDateTime.svelte
+++ src/components/event/LocalDateTime.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
     export let date: Date;
-
+    export let date_options: Intl.DateTimeFormatOptions = {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: false,
+    };
     date = new Date(date);
 </script>
 
-<span>{date}</span>
+<span>{date.toLocaleString('en-US', date_options)}</span>
