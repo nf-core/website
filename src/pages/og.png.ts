8a79331fb3cb20f71df98d75ad6436b0c24e8deb
mashehu <mashehu3@gmail.com>
2023-11-22 15:29:51 +0100

    use fallback subtitle

--- src/pages/og.png.ts
+++ src/pages/og.png.ts
@@ -29,6 +29,7 @@ export const GET: APIRoute = async ({ params, request }) => {
     } else {
         args.title = 'nf-core';
         args.subtitle = 'A community effort to collect a curated set of analysis pipelines built using Nextflow.';
+        subtitle = args.subtitle;
     }
     const html_string = `
     <div class="container"
