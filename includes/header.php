0a3b409a654f27627bbb5d9630af0a43b5a0b830
Matthias Hörtenhuber <mashehu3@gmail.com>
2023-11-29 21:30:21 +0000

    update fontawesome kit

--- includes/header.php
+++ includes/header.php
@@ -52,7 +52,7 @@ if (isset($subtitle) && strlen($subtitle) > 0) {
   <link rel="shortcut icon" href="/assets/img/logo/nf-core-logo-square.png" type="image/png" />
   <link rel="alternate" type="application/rss+xml" title="nf-core: Events" href="/events/rss" />
   <!-- FontAwesome -->
-  <script src="https://kit.fontawesome.com/38356a05cc.js" crossorigin="anonymous"></script>
+  <script src="https://kit.fontawesome.com/a060d5f3ab.js" crossorigin="anonymous"></script>
   <!-- Global site tag (gtag.js) - Google Analytics -->
   <script async src="https://www.googletagmanager.com/gtag/js?id=UA-68098153-2"></script>
   <!-- Other JS -->
