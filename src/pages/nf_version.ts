3ef9eab5d1d769f7ed4970db0c4bf987ed446066
mashehu <mashehu3@gmail.com>
2024-01-12 10:01:19 +0100

    respond with all versions and include urls

---
+++ src/pages/nf_version.ts
@@ -0,0 +1,36 @@
+import type { APIRoute } from 'astro';
+
+export const GET: APIRoute = async ({ params, request }) => {
+    try {
+        // get latest stable nextflow version from github releases
+        const response = await fetch('https://api.github.com/repos/nextflow-io/nextflow/releases');
+        if (!response.ok) {
+            throw new Error('Failed to fetch latest version');
+        }
+        const versions = await response.json();
+        const formattedVersions = versions.map((version: any) => ({
+            'version': version['tag_name'],
+            'isEdge': version['prerelease'],
+            'downloadUrl': version['assets'][0]['browser_download_url'],
+            'downloadUrlAll': version['assets'][1]['browser_download_url'],
+            'sha256': version['assets'][0]['browser_download_url']
+        }));
+        console.log(formattedVersions);
+        return new Response(
+            JSON.stringify({
+                'versions': formattedVersions
+            }),{
+      headers: {
+        "content-type": "application/json;charset=UTF-8",
+      },
+    }
+        );
+    } catch (error) {
+        return new Response(
+            JSON.stringify({
+                'error': error.message
+            }),
+            { status: 500 }
+        );
+    }
+}
