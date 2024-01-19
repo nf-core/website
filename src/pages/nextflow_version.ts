8be3732b37f8ee7e96a4b3fdb735d97188cb66ff
mashehu <mashehu3@gmail.com>
2024-01-19 09:34:41 +0100

    list ALL versions

--- src/pages/nextflow_version.ts
+++ src/pages/nextflow_version.ts
@@ -1,20 +1,48 @@
 import type { APIRoute } from 'astro';
+import octokit from '@components/octokit';
+import type { OctokitResponse } from '@octokit/types';
 
 export const GET: APIRoute = async ({ params, request }) => {
     try {
-        // get latest stable nextflow version from github releases
-        const response = await fetch('https://api.github.com/repos/nextflow-io/nextflow/releases');
-        if (!response.ok) {
-            throw new Error('Failed to fetch latest version');
-        }
-        const versions = await response.json();
+        // get all releases as versions, go through all release pages
+
+        const versions: {}[] = [];
+        let page = 1;
+        let releases: OctokitResponse<any>;
+        do {
+            releases = await octokit.request('GET /repos/{owner}/{repo}/releases', {
+                owner: 'nextflow-io',
+                repo: 'nextflow',
+                page,
+            });
+            versions.push(...releases.data);
+            page++;
+        } while (releases.headers.link?.includes('rel="next"'));
+
+
+
         const formattedVersions = versions.map((version: any) => ({
             version: version['tag_name'],
             isEdge: version['prerelease'],
-            downloadUrl: version['assets'][0]['browser_download_url'],
-            downloadUrlAll: version['assets'][1]['browser_download_url'],
+            downloadUrl: version['assets'][0] && version['assets'][0]['browser_download_url'],
+            downloadUrlAll: version['assets'][1] && version['assets'][1]['browser_download_url'],
             published_at: version['published_at'],
         }));
+        console.log(new Response(
+            JSON.stringify({
+                latest:{
+                    stable: formattedVersions.find((version: any) => !version.isEdge),
+                    edge: formattedVersions.find((version: any) => version.isEdge),
+                    everything: formattedVersions[0],
+                },
+                versions: formattedVersions,
+            }), {
+        status: 200,
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    }
+        ));
         return new Response(
             JSON.stringify({
                 latest:{
@@ -23,7 +51,12 @@ export const GET: APIRoute = async ({ params, request }) => {
                     everything: formattedVersions[0],
                 },
                 versions: formattedVersions,
-            })
+            }), {
+        status: 200,
+        headers: {
+        "Content-Type": "application/json"
+        }
+    }
         );
     } catch (error) {
         return new Response(
