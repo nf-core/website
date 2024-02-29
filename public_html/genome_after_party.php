7beb3f73bc0bbd51052c9ca08cb4131684b704eb
Matthieu Muffato <mm49@sanger.ac.uk>
2024-02-29 17:01:27 +0000

    Updated title

--- public_html/genome_after_party.php
+++ public_html/genome_after_party.php
@@ -1,6 +1,6 @@
 <?php
 $title = 'Genome After Party';
-$subtitle = 'Current and planned features for the pipelines included in the Genome After Party suite.';
+$subtitle = 'Genome analysis pipelines.';
 $markdown_fn = '../markdown/projects/genome_after_party.md';
 $md_github_url = 'https://github.com/sanger-tol/pipelines-website/blob/main/markdown/projects/genome_after_party.md';
 include '../includes/header.php';
