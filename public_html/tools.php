a145394d5bf800bbdcd9a999c4c4768e057192fa
Matthieu Muffato <mm49@sanger.ac.uk>
2024-02-29 17:53:51 +0000

    typo

--- public_html/tools.php
+++ public_html/tools.php
@@ -5,6 +5,6 @@ $subtitle = 'The sanger-tol tools that power our pipelines.';
 $markdown_fn = '../markdown/resources/tools.md';
 $md_github_url = 'https://github.com/sanger-tol/pipelines-website/blob/main/markdown/resources/tools.md';
 
-include '../../includes/header.php';
-include '../../includes/footer.php';
+include '../includes/header.php';
+include '../includes/footer.php';
 ?>
