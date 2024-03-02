803c6108729797ac0c071ff1a30e01ae60765cb2
Phil Ewels <phil.ewels@seqera.io>
2024-03-02 23:24:11 +0100

    New shop index page and locale specific embed

--- src/config.ts
+++ src/config.ts
@@ -3,3 +3,31 @@
 export const SITE_TITLE = 'nf-core';
 export const SITE_DESCRIPTION =
     'A community effort to collect a curated set of analysis pipelines built using Nextflow.';
+
+// Details from
+// https://help.spreadshop.support/hc/en-us/articles/360010529039-Website-Integration-with-JavaScript
+export const spreadshopLocales = [
+    ['🇺🇸 United States: English, Dollar ($)', 'US', 'com'],
+    ['🇬🇧 United Kingdom: English, Pound Sterling (£)', 'EN_GB', 'net'],
+    ['🇩🇪 Germany: German, Euro (€)', 'DE', 'net'],
+    ['🇪🇸 Spain: Spanish, Euro (€)', 'ES', 'net'],
+    ['🇫🇷 France: French, Euro (€)', 'FR', 'net'],
+    ['🇸🇪 Sweden: Swedish, Swedish krone (kr)', 'SE', 'net'],
+    ['🇮🇹 Italy: Italian, Euro (€)', 'IT', 'net'],
+    ['🇦🇺 Australia: English, Australian Dollar (AUD$)', 'AU', 'com'],
+    ['🇨🇦 Canada: English, Dollar ($)', 'CA', 'com'],
+    ['🇨🇦 Canada: French, Canadian Dollar (CAD$)', 'CA', 'com'],
+    ['🇳🇱 Netherlands: Dutch, Euro (€)', 'NL', 'net'],
+    ['🇨🇭 Switzerland: French, Swiss Franc (CHF)', 'CH', 'net'],
+    ['🇨🇭 Switzerland: German, Swiss Franc (CHF)', 'CH', 'net'],
+    ['🇨🇭 Switzerland: Italian, Swiss Franc (CHF)', 'CH', 'net'],
+    ['🇩🇰 Denmark: Danish, Danish krone (kr)', 'DK', 'net'],
+    ['🇦🇹 Austria: German, Euro (€)', 'AT', 'net'],
+    ['🇧🇪 Belgium: Dutch, Euro (€)', 'BE', 'net'],
+    ['🇧🇪 Belgium: French, Euro (€)', 'BE', 'net'],
+    ['🇫🇮 Finland: Finnish, Euro (€)', 'FI', 'net'],
+    ['🇵🇱 Poland: Polish, Euro (€)', 'PL', 'net'],
+    ['🇳🇴 Norway: Norwegian, Norwegian krone (kr)', 'NO', 'net'],
+    ['🇮🇪 Ireland: English, Euro (€)', 'EN_IE', 'net'],
+    ['🇪🇺 Europe: English, Euro (€)', 'EN_EU', 'net'],
+];
