9b7958642efa83cf29f98fbb98f12b1a6ab543f7
mashehu <mashehu3@gmail.com>
2024-03-05 17:08:15 +0100

    fix extended github profile pictures when container is too small

--- src/components/GitHubProfilePictureExtended.svelte
+++ src/components/GitHubProfilePictureExtended.svelte
@@ -5,25 +5,35 @@
     export let username: string;
     export let affiliation: string = '';
     export let size: number = 50;
+    export let wrapperClasses: string = '';
 
     const affiliation_str =
         affiliation.length > 0
-            ? `<div class="small"><abbr class="small d-inline-block text-truncate" data-bs-placement="bottom" data-bs-toggle="tooltip" title="${affiliation}" style="max-width: 12rem;">${affiliation}</abbr></div>`
+            ? `<div class="small"><abbr class="small d-block text-truncate" data-bs-placement="bottom" data-bs-toggle="tooltip" title="${affiliation}" style="max-width: 12rem;">${affiliation}</abbr></div>`
             : '';
-    const extra_classes = affiliation.length > 0 ? 'small' : '';
+    const extraClasses = affiliation.length > 0 ? 'small' : '';
 </script>
 
-<GitHubProfilePicture
-    wrapperClasses="btn btn-light rounded-pill mb-2 p-0 pe-2 d-inline-flex align-items-center"
-    image={'https://github.com/' + username + '.png'}
-    name={username}
-    circle={true}
-    size={Math.max(size, 25)}
->
-    <div class={'ms-2 text-start d-inline-flex flex-column px-2 ' + extra_classes}>
-        <slot>
-            <div>@{username}</div>
-        </slot>
-        {@html affiliation_str}
-    </div>
-</GitHubProfilePicture>
+<div>
+    <GitHubProfilePicture
+        {wrapperClasses}
+        linkClasses="btn btn-light rounded-pill mb-2 p-0 d-flex align-items-center"
+        image={'https://github.com/' + username + '.png'}
+        name={username}
+        circle={true}
+        size={Math.max(size, 25)}
+    >
+        <div class={'ms-2 pe-2 text-start d-flex flex-column profile-name ' + extraClasses}>
+            <slot>
+                @{username}
+            </slot>
+            {@html affiliation_str}
+        </div>
+    </GitHubProfilePicture>
+</div>
+
+<style lang="scss">
+    .profile-name :global(p) {
+        margin-bottom: 0;
+    }
+</style>
