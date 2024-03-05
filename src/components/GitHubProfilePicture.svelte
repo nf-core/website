9defe45df03ccef85ffefa7ed18425af0ed2c01c
mashehu <mashehu3@gmail.com>
2024-03-05 17:08:15 +0100

    fix extended github profile pictures when container is too small

--- src/components/GitHubProfilePicture.svelte
+++ src/components/GitHubProfilePicture.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
     export let name: string;
     export let count: number = 0;
-    export let image: string;
+    export let image: string = 'https://github.com/' + name + '.png';
     export let size: number = 60;
     export let circle: boolean = false;
+    export let linkClasses = '';
     export let wrapperClasses = '';
     export let imgClasses = '';
 
@@ -11,31 +12,51 @@
     const avatar_url = image.match(/\?/) ? `${image}&s=${size}` : `${image}?s=${size}`;
 </script>
 
-<a
-    href="https://github.com/{name}"
-    class={'text-decoration-none me-1  ' + wrapperClasses}
-    target="_blank"
-    rel="noopener noreferrer"
->
-    <img
-        src={avatar_url}
-        width={size}
-        height={size}
-        class:rounded-circle={circle}
-        data-bs-placement="bottom"
-        data-bs-toggle="tooltip"
-        title={tooltip}
-        alt={`Github user ${name}`}
-        style="--size:{size};"
-        class={imgClasses}
-    />
-    <slot />
-</a>
+<div class={'github-profile d-flex ' + wrapperClasses}>
+    <a
+        href="https://github.com/{name}"
+        class={'text-decoration-none d-block overflow-scroll ' + linkClasses}
+        target="_blank"
+        rel="noopener noreferrer"
+    >
+        <img
+            src={avatar_url}
+            width={size}
+            height={size}
+            class:rounded-circle={circle}
+            data-bs-placement="bottom"
+            data-bs-toggle="tooltip"
+            title={tooltip}
+            alt={`Github user ${name}`}
+            style="--size:{size};"
+            class={'align-self-end ' + imgClasses}
+        />
+        <div class="profile-name text-nowrap">
+            <slot />
+        </div>
+    </a>
+</div>
 
 <style lang="scss">
-    @import '@styles/_variables.scss';
+    @import '../styles/_variables.scss';
     img {
-        max-height: calc(var(--size) * 1px);
+        min-height: calc(var(--size) * 1px);
+        height: 100%;
         background-color: $white;
+        min-width: calc(var(--size) * 1px);
+    }
+    .github-profile {
+        container-type: inline-size;
+        container-name: github-profile;
+        width: 100%;
+        // min-width: 15rem;
+    }
+    .github-profile a {
+        width: fit-content;
+    }
+    @container github-profile (width < 5rem) {
+        .profile-name {
+            display: none;
+        }
     }
 </style>
