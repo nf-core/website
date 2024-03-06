542ee6709eeb03b2864395599052dff3558634f8
mashehu <mashehu3@gmail.com>
2024-03-06 11:19:10 +0100

    fix event indicator

--- src/components/event/EventIndicator.svelte
+++ src/components/event/EventIndicator.svelte
@@ -9,13 +9,12 @@
 {/if}
 
 <style lang="scss">
-    @import '@styles/_variables.scss';
-    .event-indicator :global(svg[astro-icon]) {
-        fill: $danger;
+    .event-indicator :global(svg[data-icon]) {
+        color: var(--bs-danger);
         width: 10pt;
         margin-bottom: 0.1rem;
     }
-    .event-indicator :global(svg[astro-icon] .fa-secondary) {
+    .event-indicator :global(svg[data-icon] .fa-secondary) {
         opacity: 0.4;
     }
 </style>
