9c6f08ac159a38476b5e6c97f23b1feaca4d4235
mashehu <mashehu3@gmail.com>
2024-03-22 16:16:58 +0100

    use wider margins, mute font-color

--- src/components/sidebar/SidebarToc.svelte
+++ src/components/sidebar/SidebarToc.svelte
@@ -21,11 +21,6 @@
 
     minHeadingDepth = Math.min(...headings.map((h) => h.depth));
 
-    // make margin classes from min to max heading depth
-    let headingMargin = {};
-    for (let i = minHeadingDepth; i <= 4; i++) {
-        headingMargin[i] = ' ps-' + (i - minHeadingDepth);
-    }
     let activeHeading = {};
     onMount(() => {
         // set the first heading as active on initial load
@@ -57,7 +52,7 @@
                 <ul class="mb-0 mt-1">
                     {#each headings as heading (heading)}
                         <li
-                            class={'nav-item' + headingMargin[heading.depth]}
+                            class={'nav-item heading-padding-' + (heading.depth - minHeadingDepth)}
                             class:active={heading.slug === activeHeading}
                             class:collapse={heading.hidden && !$showHidden}
                         >
@@ -120,18 +115,32 @@
     :global([data-bs-theme='dark']) {
         li {
             border-inline-start: 2pt solid $border-color-dark;
-            & a:hover {
+            &:hover {
                 background-color: transparentize($success-dark, 0.6);
-
-                color: $gray-200 !important;
+                & a {
+                    color: $gray-300 !important;
+                }
             }
         }
         li.active {
             border-left: 2pt solid $success-dark;
             background-color: transparentize($success-dark, 0.35);
             & a {
-                color: $gray-200 !important;
+                color: $gray-300 !important;
             }
+        }
+    }
+    @mixin heading-padding($number) {
+        @if $number >= 0 and $number <= 6 {
+            padding-left: #{$number * 1rem};
+        } @else {
+            @warn "Invalid input. Please provide a number between 1 and 6.";
+        }
+    }
+
+    @for $i from 0 through 6 {
+        .heading-padding-#{$i} {
+            @include heading-padding($i);
         }
     }
 </style>
