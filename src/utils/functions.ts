7684113bce917a8d62e0b4ccb1ad47bee954e471
mashehu <mashehu3@gmail.com>
2024-04-13 10:25:58 +0200

    share function between components

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,25 +1,56 @@
 import type { SidebarEntry } from '@utils/types';
-export const createLinkOrGroup = (part, path, isLastPart, currentUrl) => {
+export const createLinkOrGroup = (
+    part: string,
+    path: string,
+    isLastPart: boolean,
+    currentUrl: string,
+    weight?: number,
+) => {
     let entry: SidebarEntry = {
         label: part,
         href: path,
         isCurrent: currentUrl.replace('.html', '') === path,
         type: isLastPart ? 'link' : 'group',
+        weight: weight,
     };
 
     if (entry.type === 'group') {
         entry.entries = [];
-        entry.collapsed = true; // You can set this dynamically as well
+        entry.collapsed = true;
     }
 
     return entry;
 };
 
-const flattenSidebar = (sidebar: SidebarEntry[]): SidebarEntry[] => {
+export const flattenSidebar = (sidebar: SidebarEntry[]): SidebarEntry[] => {
     if (!sidebar) {
         return [];
     }
     return sidebar.flatMap((entry) =>
         entry.type === 'group' && entry.entries ? flattenSidebar(entry.entries) : entry,
     );
+};
+
+export const findCurrentGroup = (sections: SidebarEntry[]) => {
+    let currentGroup: SidebarEntry[] = [];
+    console.log('sections', sections);
+    sections.forEach((section) => {
+        if (section.type === 'group' && section.entries) {
+            console.log('parsing group', section.label);
+            section.entries.forEach((entry) => {
+                if (entry.isCurrent) {
+                    currentGroup = section.entries;
+                } else if (entry.type === 'group' && entry.entries) {
+                    console.log('going deeper into group', entry.label);
+                    const group = findCurrentGroup(entry.entries);
+                    if (group.length > 0) {
+                        currentGroup = group;
+                    }
+                }
+            });
+        } else if (section.isCurrent) {
+            currentGroup = sections;
+        }
+    });
+    return currentGroup;
 };
