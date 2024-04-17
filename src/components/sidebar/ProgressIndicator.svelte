31e1b27d13950e940450fde6cc7823130a643695
mashehu <mashehu3@gmail.com>
2024-04-17 11:33:40 +0200

    save checkbox status

---
+++ src/components/sidebar/ProgressIndicator.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { onMount } from 'svelte';
+
+    export let progress = 0;
+
+    onMount(() => {});
+</script>
+
+<div class="progress-bar">
+    <div class="progress" style="width: {progress}%"></div>
+</div>
+
+<style>
+    .progress-bar {
+        width: 100%;
+        height: 2px;
+        background-color: var(--bs-light);
+    }
+
+    .progress-bar .progress {
+        height: 100%;
+        background-color: var(--bs-success);
+        transition: width 0.3s;
+    }
+</style>
