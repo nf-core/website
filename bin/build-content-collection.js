45c034e48cbd1b754aab754383c6e68fcb3cfbbe
mashehu <mashehu3@gmail.com>
2024-04-30 14:35:43 +0200

    fix path in build-content-collection

--- bin/build-content-collection.js
+++ bin/build-content-collection.js
@@ -25,30 +25,34 @@ async function buildContentCollection() {
 
       console.log('current path:', path.resolve());
       const files = getAllMDFiles('.cache');
-      if (!existsSync('./sites/pipelines/src/content/pipelines')) {
-        mkdirSync('./sites/pipelines/src/content/pipelines', { recursive: true });
-        console.log('Created sites/pipelines/src/content/pipelines folder');
-        console.log('current path:', path.resolve());
+      const targetPath = path.resolve().endsWith('sites/pipelines')
+        ? 'src/content/pipelines'
+        : 'sites/pipelines/src/content/pipelines';
+      console.log('target path:', targetPath);
+      if (!existsSync(targetPath)) {
+        mkdirSync(targetPath, { recursive: true });
+        console.log('Created ', targetPath, ' folder');
       }
-  Promise.all(
-    // create sites/pipelines/src/content/pipelines folder if it doesn't exist
+      Promise.all(
+        // create sites/pipelines/src/content/pipelines folder if it doesn't exist
 
-    files.map(async (f) => {
-      let content = await promises.readFile(f, 'utf8');
-      const pathParts = f.split('/');
-      const pipeline = pathParts[1];
-      const version = pathParts[2];
-      // make relative links to png and svg files absolute in markdown to current Astro.url.pathname
-      content = content.replaceAll(
-        /(\]\()(docs\/images\/.*?\.png|svg)/gmu,
-        `$1${`https://raw.githubusercontent.com/nf-core/${pipeline}/${version}/$2`}`
+        files.map(async (f) => {
+          let content = await promises.readFile(f, 'utf8');
+          const pathParts = f.split('/');
+          const pipeline = pathParts[1];
+          const version = pathParts[2];
+          // make relative links to png and svg files absolute in markdown to current Astro.url.pathname
+          content = content.replaceAll(
+            /(\]\()(docs\/images\/.*?\.png|svg)/gmu,
+            `$1${`https://raw.githubusercontent.com/nf-core/${pipeline}/${version}/$2`}`,
+          );
+          const newPath = f.replace('.cache', targetPath);
+          const parent = newPath.split('/').slice(0, -1).join('/');
+          await promises.mkdir(parent, { recursive: true });
+          console.log('Moving ', f, ' to ', newPath);
+          await promises.writeFile(newPath, content);
+        }),
       );
-      const newPath = f.replace('.cache', 'sites/pipelines/src/content/pipelines');
-      const parent = newPath.split('/').slice(0, -1).join('/');
-      await promises.mkdir(parent, { recursive: true });
-      await promises.writeFile(newPath, content);
-    })
-  );
   return true;
 };
 
