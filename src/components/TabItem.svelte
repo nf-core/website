2e284f04691f38a09bc59654ee9904ed4bebfae9
mashehu <mashehu3@gmail.com>
2024-05-02 16:25:20 +0200

    use svelte based  tab logic instead of bootstrap's
    couldn't get it to sync nicely

--- src/components/TabItem.svelte
+++ src/components/TabItem.svelte
@@ -5,8 +5,10 @@
 
 <div
     class={`tab-pane fade border border-top-0 p-2 ${name}-tab-pane`}
+    id={`${name}-tab-pane`}
     class:active={name === $CurrentTab}
     class:show={name === $CurrentTab}
+    class:collapse={name !== $CurrentTab}
     role="tabpanel"
     aria-labelledby={`${name}-tab`}
     tabindex="0"
--- src/components/Tabs.svelte
+++ src/components/Tabs.svelte
@@ -1,26 +1,25 @@
 <script lang="ts">
     import { CurrentTab } from '@components/store';
     export let names: string[] = [];
-
-    CurrentTab.set(names[0]);
-    if (!CurrentTab) {
-        console.log('CurrentTab', CurrentTab);
-    }
 </script>
 
-<ul class="nav nav-tabs" role="tablist">
+<ul class="nav nav-tabs nav-justified" role="tablist">
     {#each names as name, index}
         <li class="nav-item" role="presentation">
             <button
                 class="nav-link border-bottom-0"
                 class:active={name === $CurrentTab}
                 id={`#${name}-tab`}
-                data-bs-toggle="tab"
-                data-bs-target={`.${name}-tab-pane`}
                 type="button"
                 role="tab"
                 aria-controls={`${name}-tab-pane`}
-                aria-selected={name === $CurrentTab}>{name}</button
+                aria-selected={name === $CurrentTab}
+                on:click={() => CurrentTab.set(name)}
+                on:keydown={(e) => {
+                    if (e.key === 'Enter') {
+                        CurrentTab.set(name);
+                    }
+                }}>{name}</button
             >
         </li>
     {/each}
