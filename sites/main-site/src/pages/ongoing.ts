79d984a71bd9dc8f0a0a35b7ddc10e7b3c3bfe6f
mashehu <mashehu3@gmail.com>
2024-06-25 14:34:30 +0200

    simplify code

--- sites/main-site/src/pages/ongoing.ts
+++ sites/main-site/src/pages/ongoing.ts
@@ -6,18 +6,12 @@ export const prerender = false;
 export const GET: APIRoute = async ({}) => {
     const now = new Date(Date.now());
     let newPost: CollectionEntry<'blog'> | undefined;
-    newPost = await getCollection('blog').then((posts) => {
-        return posts.find(
-            (post) =>
-                now > new Date(post.data.pubDate) &&
-                new Date(post.data.pubDate) > new Date(now.getTime() - 1 * 24 * 60 * 60 * 1000),
-        );
-    });
-    if (newPost) {
-        newBlogPost.set(true);
-    } else {
-        newBlogPost.set(false);
-    }
+    newPost = (await getCollection('blog')).find(
+        (post) =>
+            now > new Date(post.data.pubDate) &&
+            new Date(post.data.pubDate) > new Date(now.getTime() - 1 * 24 * 60 * 60 * 1000),
+    );
+    newBlogPost.set(Boolean(newPost));
 
     let events: CollectionEntry<'events'>[] = await getCollection('events');
     events = events
@@ -26,20 +20,14 @@ export const GET: APIRoute = async ({}) => {
             event.data.end = new Date(event.data.endDate + 'T' + event.data.endTime);
             return event;
         })
-        .sort((a, b) => {
-            return a.data.start - b.data.start;
-        });
-    events = events.filter((event) => {
-        return event.slug.split('/').length === 2;
-    });
-    const ongoingEvents = (events = events.filter((event) => {
-        return event.data.start.getTime() < now.getTime() && event.data.end.getTime() > now.getTime();
-    }));
-    if (ongoingEvents.length > 0) {
-        EventIsOngoing.set(true);
-    } else {
-        EventIsOngoing.set(false);
-    }
+        .sort((a, b) => a.data.start - b.data.start)
+        .filter((event) => event.slug.split('/').length === 2);
+
+    const ongoingEvents = events.filter(
+        (event) => event.data.start.getTime() < now.getTime() && event.data.end.getTime() > now.getTime(),
+    );
+
+    EventIsOngoing.set(ongoingEvents.length > 0);
 
     return new Response(JSON.stringify({ newPost: newPost, ongoingEvents: ongoingEvents }), {
         status: 200,
