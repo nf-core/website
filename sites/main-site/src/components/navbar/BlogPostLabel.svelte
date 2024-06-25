62c4996f979b8e163cc481d53041fbdc0311076a
mashehu <mashehu3@gmail.com>
2024-06-25 14:32:42 +0200

    add `ongoing.ts` as an endpoint to sync navbar indicators between microsites

---
+++ sites/main-site/src/components/navbar/BlogPostLabel.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { newBlogPost } from '@components/store';
+</script>
+
+{#if newBlogPost}
+    <span
+        class="float-end text-primary-emphasis border border-primary badge rounded-pill fw-normal text btn-sm text-nowrap"
+    >
+        new post
+    </span>
+{/if}
---
+++ sites/main-site/src/components/navbar/EventIndicator.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import { EventIsOngoing, newBlogPost } from '@components/store';
+
+    export let type: 'event' | 'blog';
+
+    let show = false;
+
+    if (type === 'event' && EventIsOngoing) {
+        show = true;
+    } else if (type === 'blog' && newBlogPost) {
+        show = true;
+    }
+</script>
+
+<div class={`event-indicator align-center d-inline-block ${type}`} class:d-none={!show}>
+    <slot />
+</div>
+
+<style lang="scss">
+    .event-indicator :global(svg[data-icon]) {
+        width: 10pt;
+        margin-bottom: 0.1rem;
+    }
+    .event-indicator :global(svg[data-icon] .fa-secondary) {
+        opacity: 0.4;
+    }
+    .event-indicator.event {
+        color: var(--bs-danger);
+    }
+    .event-indicator.blog {
+        color: var(--bs-primary);
+    }
+</style>
