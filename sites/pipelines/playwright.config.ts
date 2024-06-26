8f9748164b3df2d25adad629a6474c188ada2d4b
mashehu <mashehu3@gmail.com>
2024-06-26 11:50:40 +0200

    add tests for config microsite

--- sites/pipelines/playwright.config.ts
+++ sites/pipelines/playwright.config.ts
@@ -3,12 +3,6 @@ import { defineConfig, devices } from '@playwright/test';
 import baseConfig from '../../playwright.config';
 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
