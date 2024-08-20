6c0a603ab61a98ecd445575376066295acd4c96f
Edmund Miller <git@edmundmiller.dev>
2024-08-20 12:16:03 -0500

    build: Add stats page work so far

---
+++ sites/stats/sources/nfcore_db/pipeline_metrics.sql
@@ -0,0 +1,7 @@
+SELECT timestamp,
+SUM(views) AS sum_total_views,
+SUM(views_uniques) AS sum_total_views_uniques,
+SUM(clones) AS sum_total_clones,
+SUM(clones_uniques) AS sum_total_clones_uniques,
+MIN(timestamp) AS min_timestamp FROM github_traffic_stats
+INNER JOIN nfcore_pipelines ON github_traffic_stats.pipeline_id = nfcore_pipelines.id GROUP BY timestamp ORDER BY timestamp ASC;
