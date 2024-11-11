5a2350f1bbc4b327f80acf072d39e9569e0b6069
mashehu <mashehu3@gmail.com>
2024-11-11 13:23:51 +0100

    silence mixed-decls warnings

--- sites/main-site/astro.config.mjs
+++ sites/main-site/astro.config.mjs
@@ -111,7 +111,7 @@ export default defineConfig({
             preprocessorOptions: {
                 scss: {
                     api: 'modern-compiler',
-                    silenceDeprecations: ['legacy-js-api'],
+                    silenceDeprecations: ['legacy-js-api','mixed-decls'],
                 },
             },
         },
