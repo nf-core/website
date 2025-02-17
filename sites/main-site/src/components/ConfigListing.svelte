54d5f98f1e68ae40e2125f007ada6c3658840b79
mashehu <mashehu3@gmail.com>
2025-02-17 12:02:37 +0100

    use new file loader to create config collection

--- sites/main-site/src/components/ConfigListing.svelte
+++ sites/main-site/src/components/ConfigListing.svelte
@@ -1,32 +1,26 @@
 <script lang="ts">
-    import { run } from "svelte/legacy";
-
     import ListingTableHeader from "@components/ListingTableHeader.svelte";
     import { SearchQuery, SortBy } from "@components/store";
+    import { type CollectionEntry } from "astro:content";
 
     interface Props {
-        configs?: {
-            name: string;
-            content: string;
-            config: {};
-        }[];
+        configs?: CollectionEntry<"configs">[];
     }
 
-    let { configs = [] }: Props = $props();
-
+    let { configs }: Props = $props();
     SortBy.set("Name");
 
     const searchconfigs = (config) => {
         if ($SearchQuery === "") {
             return true;
         }
-        if (config.name.toLowerCase().includes($SearchQuery.toLowerCase())) {
+        if (config.id.toLowerCase().includes($SearchQuery.toLowerCase())) {
             return true;
         }
-        if (config.config_profile_description?.toLowerCase().includes($SearchQuery.toLowerCase())) {
+        if (config.rendered.metadata.config_profile_description?.toLowerCase().includes($SearchQuery.toLowerCase())) {
             return true;
         }
-        if (config.executor?.toLowerCase().includes($SearchQuery.toLowerCase())) {
+        if (config.rendered.metadata.executor?.toLowerCase().includes($SearchQuery.toLowerCase())) {
             return true;
         }
         return false;
@@ -34,14 +28,16 @@
     let invertSort = false;
     const sortConfigs = (a, b) => {
         invertSort = $SortBy.endsWith(";inverse");
+        console.log(invertSort);
         if ($SortBy.startsWith("Name")) {
-            return a.name.localeCompare(b.name) * (invertSort ? -1 : 1);
+            return a.id.localeCompare(b.id) * (invertSort ? -1 : 1);
         } else if ($SortBy.startsWith("Executor")) {
-            if (a.config.executor && b.config.executor) {
-                return a.config.executor.localeCompare(b.config.executor) * (invertSort ? -1 : 1);
-            } else if (a.config.executor) {
+            if (a.rendered.metadata.executor && b.rendered.metadata.executor) {
+                console.log(a.rendered.metadata.executor, b.rendered.metadata.executor);
+                return a.rendered.metadata.executor.localeCompare(b.rendered.metadata.executor) * (invertSort ? -1 : 1);
+            } else if (a.rendered.metadata.executor) {
                 return -1;
-            } else if (b.config.executor) {
+            } else if (b.rendered.metadata.executor) {
                 return 1;
             } else {
                 return 0;
@@ -52,27 +48,8 @@
         return configs.filter(searchconfigs).sort(sortConfigs);
     }
 
-    SearchQuery.subscribe(() => {
-        filteredConfigs = searchFilterSortConfigs(configs);
-    });
-
-    SortBy.subscribe(() => {
-        filteredConfigs = searchFilterSortConfigs(configs);
-    });
-
-    let filteredConfigs;
-    run(() => {
-        filteredConfigs = searchFilterSortConfigs(configs);
-    });
-    configs.map((config) => {
-        config.name = config.name.replace(".md", "");
-    });
-
-    const executor_icons = {
-        slurm: "slurm",
-        awsbatch: "aws",
-        azurebatch: "azure",
-    };
+    let filteredConfigs = $derived(searchFilterSortConfigs(configs));
+    $inspect(filteredConfigs[2]);
 </script>
 
 <div class="listing d-flex flex-wrap w-100 justify-content-center">
@@ -89,17 +66,17 @@
                 <tr>
                     <td class="name p-0">
                         <div class="position-relative p-3">
-                            <a class="stretched-link" href={"/configs/" + config.name + "/"}
-                                >{@html config.name.replace("_", "_<wbr>")}</a
+                            <a class="stretched-link" href={"/configs/" + config.id + "/"}
+                                >{@html config.id.replaceAll("_", "_<wbr>").replace("conf/", "")}</a
                             >
                         </div>
                     </td>
                     <td class="description text-small">
-                        {config.config.config_profile_description}
+                        {config.rendered.metadata.config_profile_description}
                     </td>
                     <td class="text-center">
-                        {#if config.config.executor}
-                            {#each config.config.executor.split(",") as executor}
+                        {#if config.rendered.metadata.executor}
+                            {#each config.rendered.metadata.executor.split(",") as executor}
                                 <span class="badge border border-secondary-subtle text-body fw-normal ms-2">
                                     {executor}
                                 </span>
