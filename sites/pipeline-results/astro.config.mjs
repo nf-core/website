1ca95fe9bec1cfdc53d8c2aa950bfc7726db1175
mashehu <mashehu3@gmail.com>
2025-03-04 17:04:12 +0100

    switch pipeline-results to content collection loader

--- sites/pipeline-results/astro.config.mjs
+++ sites/pipeline-results/astro.config.mjs
@@ -109,7 +109,7 @@ export default defineConfig({
             }),
         ],
         ssr: {
-            noExternal: ['@popperjs/core', '../../bin/cache.js'],
+            noExternal: ['@popperjs/core'],
         },
         resolve: {
             preserveSymlinks: true,
