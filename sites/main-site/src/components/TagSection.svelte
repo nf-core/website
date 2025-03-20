e9f62f9c0333606b963093cc8caa26575d73a64c
lmReef <ReefMatson@gmail.com>
2025-03-20 16:08:11 +1300

    add tag section component

---
+++ sites/main-site/src/components/TagSection.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+    interface Props {
+        tags: string[];
+        type: "modules" | "subworkflows" | "pipelines" | "keywords";
+        maxShown?: number;
+        includes?: boolean;
+        included?: boolean;
+        inline?: boolean;
+    }
+
+    const { tags, maxShown = 0, type, inline = false, includes = false, included = false }: Props = $props();
+    let expanded = $state(false);
+
+    const typeSpecificClasses =
+        type === "keywords"
+            ? "bg-body-tertiary text-success"
+            : type === "pipelines"
+              ? "border text-body border-warning-subtle bg-warning-subtle"
+              : "border text-body border-info-subtle bg-info-subtle"; // subworkflows and modules share styling
+    const additionalTagsCount = tags.length - maxShown;
+
+    function getHref(tag: string) {
+        if (type === "pipelines") return `/${tag.replace("/", "_")}/`;
+        else if (type === "keywords") return null;
+        return `/${type}/${tag.replace("/", "_")}/`;
+    }
+    function onclick() {
+        expanded = !expanded;
+    }
+</script>
+
+<div class="tag-section my-1 text-body-secondary" class:topics={type === "keywords"}>
+    {#if includes || included}
+        <span class="text-small">{includes ? "Includes:" : "Included in:"}</span>
+        {#if !inline}<br />{/if}
+    {/if}
+
+    {#each expanded || !maxShown ? tags : tags.slice(0, maxShown) as tag}
+        <a href={getHref(tag)} class="badge fw-normal me-2 text-decoration-none {typeSpecificClasses}">
+            {tag}
+        </a>
+    {/each}
+
+    {#if additionalTagsCount > 0 && maxShown}
+        {#if !inline}<br />{/if}
+        <span
+            class="text-small text-nowrap"
+            title={`click to show all ${type}`}
+            {onclick}
+            onkeydown={onclick}
+            tabindex="0"
+            role="button"
+            data-bs-toggle="tooltip"
+            data-bs-delay="500"
+        >
+            {#if expanded}
+                hide
+            {/if}
+            {#if !expanded}
+                +{additionalTagsCount} more {type}
+            {/if}
+        </span>
+    {/if}
+</div>
