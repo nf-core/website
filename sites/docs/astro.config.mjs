30842edbc5a4e532bfcbe1631e6483a1f876d48f
mashehu <mashehu3@gmail.com>
2025-10-27 11:06:51 +0100

    fix docs config

--- sites/docs/astro.config.mjs
+++ sites/docs/astro.config.mjs
@@ -1,4 +1,5 @@
 import baseConfig from "../../astro.config.base.mjs";
+import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
@@ -8,14 +9,11 @@ export default defineConfig({
     ...baseConfig.build,
     assetsPrefix: "https://nf-core-docs.netlify.app/",
   },
-  integrations: baseConfig.integrations.map((integration) => {
-    // Override icon integration to set iconDir
-    if (integration.name === "astro-icon") {
-      return integration.hooks.then((hooks) => ({
-        ...hooks,
-        config: { ...hooks.config, iconDir: "../main-site/src/icons" },
-      }));
-    }
-    return integration;
-  }),
+  integrations: [
+    ...baseConfig.integrations.filter((i) => i.name !== "astro-icon"),
+    icon({
+      iconDir: "../main-site/src/icons",
+      include: baseConfig.integrations.find((i) => i.name === "astro-icon")?.include,
+    }),
+  ],
 });
