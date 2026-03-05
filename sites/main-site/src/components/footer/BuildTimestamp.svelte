9fc6fbbdbda36bc676cba4e14ca8bc77fb2ad5aa
mashehu <mashehu3@gmail.com>
2026-03-05 11:19:01 +0100

    make timestamp relative

---
+++ sites/main-site/src/components/footer/BuildTimestamp.svelte
@@ -0,0 +1,7 @@
+<script>
+    import { formatDistanceToNow } from "date-fns";
+
+    let { timestamp } = $props();
+</script>
+
+<span class="text-muted" title={timestamp}>Built {formatDistanceToNow(new Date(timestamp), { addSuffix: true })}</span>
